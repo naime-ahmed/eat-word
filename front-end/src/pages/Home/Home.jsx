a7f5551d99a87d871a1aeb5da2544f16c232d4db
naime-ahmed <naime2molla@gmail.com>
2024-09-01 21:16:56 +0600

    included the tutorial section

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import Header from "../../components/shared/Header/Header";
-import Hero from "./Sections/Hero/Hero";
-
 import style from "./Home.module.css";
+import Hero from "./Sections/Hero/Hero";
+import Tutorials from "./Sections/Tutorials/Tutorials";
 
 const Home = () => {
   return (
     <div className={style.homeContainer}>
       <Header />
       <Hero />
+      <Tutorials />
     </div>
   );
 };
