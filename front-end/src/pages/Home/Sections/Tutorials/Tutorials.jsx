29b677d291eca5f94d109b02e4a29da01f420473
naime-ahmed <naime2molla@gmail.com>
2024-09-01 21:19:16 +0600

    designed the tutorial section responsive for all devices

--- front-end/src/pages/Home/Sections/Tutorials/Tutorials.jsx
+++ front-end/src/pages/Home/Sections/Tutorials/Tutorials.jsx
@@ -1,7 +1,25 @@
+import thumbnail from "../../../../assets/thumbnailForTutorial.png";
+import video from "../../../../assets/tmpVideoForTutorial.mp4";
+import style from "./Tutorials.module.css";
+
 const Tutorials = () => {
   return (
-    <div>
-      <h1>Watch to learn</h1>
+    <div className={style.tutorialsContainer}>
+      <div className={style.tutorialsTextContainer}>
+        <h1>
+          Get Started with <span>your words</span>
+        </h1>
+        <p>
+          Watch this short tutorial to see how you can start mastering your
+          words.
+        </p>
+      </div>
+      <div className={style.tutorialsVideoContainer}>
+        <video poster={thumbnail} controls>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 };
