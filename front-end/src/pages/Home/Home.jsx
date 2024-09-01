9493af69d43c50fc091ad947416b73d440ec677a
naime-ahmed <naime2molla@gmail.com>
2024-09-01 18:35:42 +0600

    included header and hero section

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import Header from "../../components/shared/Header/Header";
-import style from "./Home.module.css";
 import Hero from "./Sections/Hero/Hero";
+
+import style from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className={style.homePage}>
-      <div className={style.homeContainer}>
-        <Header />
-        <Hero />
-      </div>
+    <div className={style.homeContainer}>
+      <Header />
+      <Hero />
     </div>
   );
 };
