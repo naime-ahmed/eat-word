aee5380bd80af9b11b74895443f0197ecca38828
naime-ahmed <naime2molla@gmail.com>
2024-09-04 01:21:44 +0600

    included the section journey of a word

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/shared/Header/Header";
 import style from "./Home.module.css";
 import BaseTheory from "./Sections/BaseTheory/BaseTheory";
 import Hero from "./Sections/Hero/Hero";
+import JourneyOfAWord from "./Sections/JourneyOfAWord/JourneyOfAWord";
 import Tutorials from "./Sections/Tutorials/Tutorials";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <Tutorials />
       <BaseTheory />
+      <JourneyOfAWord />
     </div>
   );
 };
