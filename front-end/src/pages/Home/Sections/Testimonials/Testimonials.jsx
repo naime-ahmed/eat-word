fd65224438c84a7c1c2b8993d4ff1280ac62a7f6
naime-ahmed <naime2molla@gmail.com>
2024-09-05 23:45:45 +0600

    designed testimonials section with react-slick

--- front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
+++ front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import style from "./Testimonials.module.css";
+
 const Testimonials = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+  useEffect(() => {
+    fetch("/DB/testimonials.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTestimonials(data);
+      })
+      .catch((error) => {
+        console.log("Error fetching testimonials", error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Add some fake words</h1>
+    <div className={`${style.testimonialContainer} slider-container`}>
+      <h1>Words of praise</h1>
+      <Slider {...settings}>
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className={style.testimonialCard}>
+            <blockquote>{testimonial.review}</blockquote>
+            <p>
+              - {testimonial.name}, {testimonial.profession}
+            </p>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
