fcc593df802546d4f29ce9f1dd793f9aca46b210
naime-ahmed <naime2molla@gmail.com>
2024-09-06 23:46:01 +0600

    designed the footer component

---
+++ front-end/src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+import style from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={style.footerContainer}>
+      <div className={style.footerTop}>
+        <div className={style.brand}>
+          <img src={logo} alt="your words logo" />
+          <p>Empower Your Vocabulary</p>
+        </div>
+        <div className={style.quickLinks}>
+          <h3>Quick Links</h3>
+          <ul>
+            <li>
+              <Link to="/">Pricing</Link>
+            </li>
+            <li>
+              <Link to="/">Features</Link>
+            </li>
+            <li>
+              <Link to="/">Blog</Link>
+            </li>
+            <li>
+              <Link to="/">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={style.learnMore}>
+          <h3>Learn More</h3>
+          <ul>
+            <li>
+              <Link to="/">About Us</Link>
+            </li>
+            <li>
+              <Link to="/">FAQ</Link>
+            </li>
+            <li>
+              <Link to="/">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to="/">Terms of Service</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={style.contactInfo}>
+          <h3>Contact Us</h3>
+          <p>
+            Email:
+            <Link className={style.email} to="mailto:support@yourwords.com">
+              support@yourwords.com
+            </Link>
+          </p>
+          <p>Phone: +8801926537994</p>
+          <p>Address: Meghna, Cumilla, Bangladesh</p>
+        </div>
+      </div>
+      <div className={style.copyright}>
+        <p>© 2024 Your Words. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
