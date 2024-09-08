0a1ed17a7fcc06718794eed1ef71ef9edcf89e7c
naime-ahmed <naime2molla@gmail.com>
2024-09-09 00:43:27 +0600

    added functionality to get valid input data

--- front-end/src/pages/SignUp/SignUp.jsx
+++ front-end/src/pages/SignUp/SignUp.jsx
@@ -1,12 +1,73 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./SignUp.module.css";
 
 const SignUp = () => {
+  const [newUser, setNewUser] = useState({
+    fName: "",
+    email: "",
+    password: "",
+    surePass: "",
+    captcha: "",
+    agree: false,
+  });
+
+  const [newUserErrors, setNewUserError] = useState({});
+
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setNewUser({ ...newUser, [name]: value });
+  }
+
+  function validateFrom() {
+    let errors = {};
+    if (!newUser.fName) errors.fName = "Full name is required";
+    if (!newUser.email || !/\S+@\S+\.\S+/.test(newUser.email)) {
+      errors.email = "Valid email is required";
+    }
+
+    if (!newUser.password || newUser.password.length < 6) {
+      errors.password = "Too weak password";
+    }
+    if (!newUser.surePass || newUser.password != newUser.surePass) {
+      errors.surePass = "Password dose not match";
+    }
+    if (!newUser.captcha || newUser.captcha !== "8") {
+      errors.captcha = "Captcha failed!";
+    }
+    if (!newUser.agree) errors.agree = "You must agree to continue";
+
+    setNewUserError(errors);
+    return Object.keys(errors).length === 0;
+  }
+
+  function handelSubmit(event) {
+    event.preventDefault();
+
+    if (validateFrom()) {
+      console.log(newUser);
+      // Reset the form to default values
+      setNewUser({
+        fName: "",
+        email: "",
+        password: "",
+        surePass: "",
+        captcha: "",
+        agree: false,
+      });
+    }
+  }
   return (
     <div className={style.signUpContainer}>
       <div className={style.formContainer}>
         <h2>Create Your Account</h2>
-        <form action="">
+        <form onSubmit={handelSubmit}>
           <div>
             <label htmlFor="fName">Your full name </label>
             <span>&#8727;</span>
@@ -16,8 +77,11 @@ const SignUp = () => {
               name="fName"
               id="fName"
               placeholder="Enter Full Name"
+              onChange={handleChange}
+              value={newUser.fName}
               required
             />
+            {newUserErrors.fName && <p>{newUserErrors.fName}</p>}
           </div>
           <div>
             <label htmlFor="email">Your email</label>
@@ -28,9 +92,15 @@ const SignUp = () => {
               name="email"
               id="email"
               placeholder="Enter email"
+              onChange={handleChange}
+              value={newUser.email}
               required
             />
+            {newUserErrors.email && (
+              <p style={{ color: "#821131" }}>{newUserErrors.email}</p>
+            )}
           </div>
+
           <div>
             <label htmlFor="password">your password</label>
             <span>&#8727;</span>
@@ -40,8 +110,13 @@ const SignUp = () => {
               name="password"
               id="password"
               placeholder="Enter password"
+              onChange={handleChange}
+              value={newUser.password}
               required
             />
+            {newUserErrors.password && (
+              <p style={{ color: "#821131" }}>{newUserErrors.password}</p>
+            )}
           </div>
           <div>
             <label htmlFor="surePass">Confirm Password</label>
@@ -52,8 +127,13 @@ const SignUp = () => {
               name="surePass"
               id="surePass"
               placeholder="confirm password"
+              onChange={handleChange}
+              value={newUser.surePass}
               required
             />
+            {newUserErrors.surePass && (
+              <p style={{ color: "#821131" }}>{newUserErrors.surePass}</p>
+            )}
           </div>
           <div>
             <label htmlFor="captcha">What is 3 + 5?</label>
@@ -64,15 +144,27 @@ const SignUp = () => {
               name="captcha"
               id="captcha"
               placeholder="Answer"
+              onChange={handleChange}
+              value={newUser.captcha}
               required
             />
+            {newUserErrors.captcha && (
+              <p style={{ color: "#821131" }}>{newUserErrors.captcha}</p>
+            )}
           </div>
           <div className={style.termsBox}>
-            <input type="checkbox" name="terms" id="terms" />
-            <label htmlFor="terms">
-              I agree to the terms and conditions.
-            </label>{" "}
+            <input
+              type="checkbox"
+              name="agree"
+              id="terms"
+              checked={newUser.agree}
+              onChange={handleChange}
+            />
+            <label htmlFor="terms">I agree to the terms and conditions.</label>{" "}
             <span>&#8727;</span>
+            {newUserErrors.agree && (
+              <p style={{ color: "#821131" }}>{newUserErrors.agree}</p>
+            )}
           </div>
           <div className={style.submitBtn}>
             <button type="submit">Submit</button>
