60cf0008d6c1895546d0f4d9161f2b9b1aaf7596
naime-ahmed <naime2molla@gmail.com>
2024-09-13 22:55:52 +0600

    wrapped the whole sys with redux provider to provide state everywhere

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import store from "./app/store";
 import "./index.css";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
-import SingIn from "./pages/SignIn/SignIn";
+import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
@@ -20,12 +22,14 @@ const router = createBrowserRouter([
   },
   {
     path: "/signIn",
-    element: <SingIn />,
+    element: <SignIn />,
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
