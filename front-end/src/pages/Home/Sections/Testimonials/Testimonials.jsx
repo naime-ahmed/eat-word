51840df7bd4f9a940bf6218ee7c781eff7646ffa
naime-ahmed <naime2molla@gmail.com>
2024-09-15 23:25:50 +0600

    added heading text

--- front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
+++ front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
@@ -16,26 +16,37 @@ const Testimonials = () => {
 
   return (
     <div className={styles.testimonialsContainer}>
-      <div className={styles.testimonialsMarquee}>
-        <div className={styles.marqueeItems}>
-          {testimonials.map((item, index) => (
-            <div key={index} className={styles.testimonialItem}>
-              <div className={styles.testimonialCard}>
-                <div className={styles.testimonialText}>{item.review}</div>
-                <div className={styles.testimonialProfile}>
-                  <img
-                    src={item.image}
-                    className={styles.profileImage}
-                    alt={item.name}
-                  />
-                  <div className={styles.profileInfo}>
-                    <div className={styles.profileName}>{item.name}</div>
-                    <div className={styles.profileTitle}>{item.profession}</div>
+      <div className={styles.testimonialHead}>
+        <h1>Words of Praise</h1>
+        <p>
+          See how &#34;Your Word&#34; is transforming vocabulary mastery for
+          learner
+        </p>
+      </div>
+      <div className={styles.testimonialsCardContainer}>
+        <div className={styles.testimonialsMarquee}>
+          <div className={styles.marqueeItems}>
+            {testimonials.map((item, index) => (
+              <div key={index} className={styles.testimonialItem}>
+                <div className={styles.testimonialCard}>
+                  <div className={styles.testimonialText}>{item.review}</div>
+                  <div className={styles.testimonialProfile}>
+                    <img
+                      src={item.image}
+                      className={styles.profileImage}
+                      alt={item.name}
+                    />
+                    <div className={styles.profileInfo}>
+                      <div className={styles.profileName}>{item.name}</div>
+                      <div className={styles.profileTitle}>
+                        {item.profession}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
