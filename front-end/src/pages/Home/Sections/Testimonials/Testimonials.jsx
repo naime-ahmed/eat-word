b220f568d9e18f9f946eebf1f1bb49787e86e025
naime-ahmed <naime2molla@gmail.com>
2024-09-16 23:36:27 +0600

    took away the core code to make it a independent component and imported it here

--- front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
+++ front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
@@ -1,19 +1,7 @@
-import { useEffect, useState } from "react";
+import Testimonial from "../../../../components/Testimonial/Testimonial";
 import styles from "./Testimonials.module.css";
 
 const Testimonials = () => {
-  const [testimonials, setTestimonials] = useState([]);
-  useEffect(() => {
-    fetch("/DB/testimonials.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setTestimonials(data);
-      })
-      .catch((error) => {
-        console.log("Error fetching testimonials", error);
-      });
-  }, []);
-
   return (
     <div className={styles.testimonialsContainer}>
       <div className={styles.testimonialHead}>
@@ -23,32 +11,8 @@ const Testimonials = () => {
           learner
         </p>
       </div>
-      <div className={styles.testimonialsCardContainer}>
-        <div className={styles.testimonialsMarquee}>
-          <div className={styles.marqueeItems}>
-            {testimonials.map((item, index) => (
-              <div key={index} className={styles.testimonialItem}>
-                <div className={styles.testimonialCard}>
-                  <div className={styles.testimonialText}>{item.review}</div>
-                  <div className={styles.testimonialProfile}>
-                    <img
-                      src={item.image}
-                      className={styles.profileImage}
-                      alt={item.name}
-                    />
-                    <div className={styles.profileInfo}>
-                      <div className={styles.profileName}>{item.name}</div>
-                      <div className={styles.profileTitle}>
-                        {item.profession}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <Testimonial lane="marqueOne" />
+      <Testimonial lane="marqueTwo" />
     </div>
   );
 };
