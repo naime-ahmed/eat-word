6cc5b3736b8dc6d844caa7702e41699b4a020336
naime-ahmed <naime2molla@gmail.com>
2024-09-18 23:51:32 +0600

    removed the tutorials section from home page

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -5,16 +5,14 @@ import BaseTheory from "./Sections/BaseTheory/BaseTheory";
 import Hero from "./Sections/Hero/Hero";
 import JourneyOfAWord from "./Sections/JourneyOfAWord/JourneyOfAWord";
 import Testimonials from "./Sections/Testimonials/Testimonials";
-import Tutorials from "./Sections/Tutorials/Tutorials";
 
 const Home = () => {
   return (
     <div className={style.homeContainer}>
       <Header />
       <Hero />
-      <Tutorials />
-      <BaseTheory />
       <JourneyOfAWord />
+      <BaseTheory />
       <Testimonials />
       <Footer />
     </div>
