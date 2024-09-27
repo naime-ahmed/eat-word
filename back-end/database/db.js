de4b373ac52234f2a4de08a37d48059b24ea6589
naime-ahmed <naime2molla@gmail.com>
2024-09-27 23:32:22 +0600

    formated the code structure

--- back-end/database/db.js
+++ back-end/database/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 const connectToDb = async () => {
-    await mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(() => {
-        console.log("Mongodb is connected");
-    }).catch((err) => console.log(err));
-}
+  await mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING)
+    .then(() => {
+      console.log("Mongodb is connected");
+    })
+    .catch((err) => console.log(err));
+};
 export default connectToDb;
