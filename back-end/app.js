58544379bad909057e99c001d2f5d4056d821510
naime-ahmed <naime2molla@gmail.com>
2024-09-29 01:05:52 +0600

    added signUp and deleteUser middleware

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,16 @@
 // external imports
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 
 // Internal imports
-import connectDb from './database/db.js';
-import { errorHandler, notFoundHandler } from './middlewares/common/errorHandler.js';
-
+import connectDb from "./database/db.js";
+import {
+  errorHandler,
+  notFoundHandler,
+} from "./middlewares/common/errorHandler.js";
+import deleteUser from "./router/usersRouter/deleteRouter.js";
+import signUpRouter from "./router/usersRouter/signUpRouter.js";
 
 const app = express();
 dotenv.config();
@@ -21,21 +25,22 @@ app.use(express.urlencoded({ extended: true }));
 // parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.get("/", (req, res) =>{
-    res.send({
-        success: true,
-        message: "Server is active",
-    })
-})
+app.use("/signUp", signUpRouter);
+app.use("/deleteUser", deleteUser);
 
+app.get("/", (req, res) => {
+  res.send({
+    success: true,
+    message: "Server is active",
+  });
+});
 
 // 404 not found handler
 app.use(notFoundHandler);
 
 // common error handler
-app.use(errorHandler) 
+app.use(errorHandler);
 // app listener
 app.listen(process.env.PORT, () => {
-    console.log(`app listening to port ${process.env.PORT}`);
-  });
-  
+  console.log(`app listening to port ${process.env.PORT}`);
+});
