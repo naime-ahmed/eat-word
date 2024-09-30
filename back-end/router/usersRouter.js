64e42e33b53f5c15afb6f1cddb533c9c02e0edc2
naime-ahmed <naime2molla@gmail.com>
2024-10-01 00:15:47 +0600

    changed some endpoint and added sign-in and sing-out endpoint

--- back-end/router/usersRouter.js
+++ back-end/router/usersRouter.js
@@ -3,13 +3,21 @@ import express from "express";
 
 // internal imports
 import { removeUser } from "../controller/usersController/deleteController.js";
+import { signIn } from "../controller/usersController/signInController.js";
+import { signOut } from "../controller/usersController/signOutController.js";
 import { addUser } from "../controller/usersController/signUpController.js";
 
 const router = express.Router();
 
-// add user for both, root and /sing-up route.
+// add user for both, root and /sign-up route.
 router.post("/", addUser);
-router.post("/sing-up", addUser);
+router.post("/sign-up", addUser);
+
+// sign in user
+router.post("/sign-in", signIn);
+
+// sign out user
+router.post("/sign-out", signOut);
 
 // delete user
 router.delete("/delete-me/:id", removeUser);
