eed9212116124679787817860befe78ce43fbbf3
naime-ahmed <naime2molla@gmail.com>
2024-10-02 22:45:56 +0600

    changed the name of software and rectify some text and images

--- front-end/src/pages/About/About.jsx
+++ front-end/src/pages/About/About.jsx
@@ -1,5 +1,5 @@
+import img1 from "../../assets/eatWordForAbout.png";
 import planImg from "../../assets/imageForAboutUsFPlan.jpg";
-import img1 from "../../assets/swallowWordShape.png";
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
 import styles from "./About.module.css";
@@ -11,20 +11,20 @@ const About = () => {
       <div className={styles.aboutUsContainer}>
         <div>
           <div>
-            <h1>What Is &#34;Swallow Word&#34;?</h1>
+            <h1>What Is &#34;Eat Word&#34;?</h1>
             <p>
-              &#34;Swallow Word&#34; is a unique vocabulary learning platform.
+              &#34;Eat Word&#34; is a unique vocabulary learning platform.
               Unlike traditional apps with pre-installed word lists, here, you
               choose the words you want to learn. Our system uses proven methods
               like Spaced Repetition, Active Recall, and Contextual Learning to
               ensure long-term retention. AI integration helps streamline the
               process by suggesting definitions, synonyms, and example
-              sentences, making learning fast and efficient. It’s a
+              sentences, making learning fast and efficient. It&#x27;s a
               personalized, science-backed approach to mastering vocabulary on
               your terms.
             </p>
           </div>
-          <div className={styles.aboutSWImg}>
+          <div className={styles.aboutEWImg}>
             <img src={img1} alt="" />
           </div>
         </div>
@@ -35,12 +35,12 @@ const About = () => {
           <div>
             <h1>Why I Created This Platform?</h1>
             <p>
-              Yes, it&#39;s built by single person. I built &#34;Swallow
-              Word&#34; for myself, as I wanted a tool that lets me take control
-              of my own learning journey. Traditional apps didn’t fit my needs,
-              so I designed this platform to focus on the words I find
-              important. Now, I’m making it public for others who, like me, want
-              a flexible, personalized approach to learning vocabulary and
+              Yes, it&#39;s built by single person. I built &#34;Eat Word&#34;
+              for myself, as I wanted a tool that lets me take control of my own
+              learning journey. Traditional apps didn&#x27;t fit my needs, so I
+              designed this platform to focus on the words I find important.
+              Now, I&#x27;m making it public for others who, like me, want a
+              flexible, personalized approach to learning vocabulary and
               improving their English proficiency.
             </p>
           </div>
@@ -53,7 +53,7 @@ const About = () => {
               progress-tracking features. Interactive tools like quizzes and
               word games will make learning more engaging. In the future, we
               also aim to foster a community where users can share words and
-              learn from each other, ensuring &#34;Swallow Word&#34; becomes a
+              learn from each other, ensuring &#34;Eat Word&#34; becomes a
               dynamic, evolving tool for learners.
             </p>
           </div>
