c61e1fc7ff42120ed849c668680751597f0a2bfa
naime-ahmed <naime2molla@gmail.com>
2024-10-06 02:05:07 +0600

    included the userApi reducer and middleware

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import signInReducers from '../features/userSignIn/userSignInSlice';
 import signUpReducers from "../features/userSignUp/userSignUpSlice";
+import { usersApi } from "../services/users";
 
 const store = configureStore({
     reducer: {
         signIn: signInReducers,
-        signUp: signUpReducers
-    }
+        signUp: signUpReducers,
+        [usersApi.reducerPath]: usersApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware),
+    devTools: import.meta.env.NODE_ENV !== 'production',
 })
 
 export default store;
