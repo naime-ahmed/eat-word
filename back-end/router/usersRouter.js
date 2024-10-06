d1b9c00115cf6cb98e0799dcb6d3fdfd2bc5bd17
naime-ahmed <naime2molla@gmail.com>
2024-10-06 21:26:57 +0600

    moved users auth realated router to authRouter file

--- back-end/router/usersRouter.js
+++ back-end/router/usersRouter.js
@@ -2,24 +2,10 @@
 import express from "express";
 
 // internal imports
-import { removeUser } from "../controller/usersController/deleteController.js";
-import { signIn } from "../controller/usersController/signInController.js";
-import { signOut } from "../controller/usersController/signOutController.js";
-import { addUser } from "../controller/usersController/signUpController.js";
 
 const router = express.Router();
 
-// add user for both, root and /sign-up route.
-router.post("/", addUser);
-router.post("/sign-up", addUser);
-
-// sign in user
-router.post("/sign-in", signIn);
-
-// sign out user
-router.post("/sign-out", signOut);
-
-// delete user
-router.delete("/delete-me", removeUser);
+// get users
+// router.get("/");
 
 export default router;
