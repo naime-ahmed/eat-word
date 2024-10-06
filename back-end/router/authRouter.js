863e4b1129562d8f409e7c08b76569448589620d
naime-ahmed <naime2molla@gmail.com>
2024-10-06 21:28:01 +0600

    dedicated only for user authentication router

---
+++ back-end/router/authRouter.js
@@ -0,0 +1,25 @@
+// external imports
+import express from "express";
+
+// internal imports
+import { signIn } from "../controller/authController/signInController.js";
+import { signOut } from "../controller/authController/signOutController.js";
+import { addUser } from "../controller/authController/signUpController.js";
+import { removeUser } from "../controller/usersController/deleteController.js";
+
+const router = express.Router();
+
+// add user for both, root and /sign-up route.
+router.post("/", addUser);
+router.post("/sign-up", addUser);
+
+// sign in user
+router.post("/sign-in", signIn);
+
+// sign out user
+router.post("/sign-out", signOut);
+
+// delete user
+router.delete("/delete-me", removeUser);
+
+export default router;
