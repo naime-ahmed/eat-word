f7d6cd010a2fcf944abe276b993c398911fedd65
naime-ahmed <naime2molla@gmail.com>
2024-10-09 01:39:42 +0600

    added authReducers

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducers from "../features/auth/authSlice";
 import signInReducers from '../features/userSignIn/userSignInSlice';
 import signUpReducers from "../features/userSignUp/userSignUpSlice";
-import { authApi } from "../services/auth.js";
+import { authApi } from "../services/auth";
 
 const store = configureStore({
     reducer: {
         signIn: signInReducers,
         signUp: signUpReducers,
+        auth: authReducers,
         [authApi.reducerPath]: authApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
