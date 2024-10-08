6ca212f1b8dc54cb9632e19c4d8a79eee100550f
naime-ahmed <naime2molla@gmail.com>
2024-10-09 01:32:03 +0600

    made the app parent of all page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,29 @@
-import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { checkAuthentication } from "./features/auth/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // get the token from local storage
+    const token = localStorage.getItem("access-token");
+    console.log(token);
+    const verifyToken = async () => {
+      if (token) {
+        await dispatch(checkAuthentication(token));
+      }
+    };
+
+    verifyToken().catch((err) => {
+      console.error("Authentication check failed", err);
+    });
+  }, [dispatch]);
+
   return (
     <>
-      <h1>Nothings, will be deleted</h1>
+      <Outlet />
     </>
   );
 }
