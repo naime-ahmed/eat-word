7b62476aec8ffc0b9da67eedcaf29e63393467e2
naime-ahmed <naime2molla@gmail.com>
2024-10-10 02:53:47 +0600

    rectify cookie clear paramenter

--- back-end/controller/authController/signOutController.js
+++ back-end/controller/authController/signOutController.js
@@ -1,10 +1,4 @@
 export function signOut(req, res) {
-  res.cookie(process.env.COOKIE_NAME, "", {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
-    maxAge: 0,
-  });
-
+  res.clearCookie(process.env.COOKIE_NAME);
   res.status(200).json({ message: "Sign-out successful" });
 }
