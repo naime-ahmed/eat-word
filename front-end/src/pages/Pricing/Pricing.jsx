9f5a5969437db894676bb754bdbee81561d470e6
naime-ahmed <naime2molla@gmail.com>
2024-10-11 22:37:05 +0600

    inited the pricing page

---
+++ front-end/src/pages/Pricing/Pricing.jsx
@@ -0,0 +1,11 @@
+import styles from './Pricing.module.css';
+
+const Pricing = () => {
+    return (
+        <div className={styles.pricingPage}>
+            <h1>Its free for you</h1>
+        </div>
+    );
+};
+
+export default Pricing;
