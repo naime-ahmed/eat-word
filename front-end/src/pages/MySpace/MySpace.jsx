db9b11e118baa5a89770179c41965470184c3c4c
naime-ahmed <naime2molla@gmail.com>
2024-10-11 22:37:47 +0600

    inited the mySpace page

---
+++ front-end/src/pages/MySpace/MySpace.jsx
@@ -0,0 +1,11 @@
+import styles from "./MySpace.module.css";
+
+const MySpace = () => {
+  return (
+    <div className={styles.myspacePage}>
+      <h1>Welcome to your vocab world</h1>
+    </div>
+  );
+};
+
+export default MySpace;
