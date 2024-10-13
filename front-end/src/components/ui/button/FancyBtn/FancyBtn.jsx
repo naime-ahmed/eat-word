0083a37a66837296d2304a985e21f9248b3987d8
naime-ahmed <naime2molla@gmail.com>
2024-10-14 03:08:29 +0600

    receving bg color to make button more dynamic

--- front-end/src/components/ui/button/FancyBtn/FancyBtn.jsx
+++ front-end/src/components/ui/button/FancyBtn/FancyBtn.jsx
@@ -5,11 +5,22 @@ const FancyBtn = ({
   clickHandler = () => {},
   btnWidth,
   btnHeight,
+  leftColor = "#0400ff",
+  rightColor = "#4ce3f7",
+  degree = "30deg",
 }) => {
+  // Implementing the linearGradient function
+  const linearGradient = (degree, leftColor, rightColor) =>
+    `linear-gradient(${degree}, ${leftColor}, ${rightColor})`;
+
   return (
     <button
       onClick={clickHandler}
-      style={{ width: btnWidth, height: btnHeight }}
+      style={{
+        width: btnWidth,
+        height: btnHeight,
+        backgroundImage: linearGradient(degree, leftColor, rightColor),
+      }}
       className={styles.fancyBtn}
     >
       {children}
