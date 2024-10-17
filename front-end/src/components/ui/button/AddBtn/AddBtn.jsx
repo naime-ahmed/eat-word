6c836b49f5da698e3a2243ebe36387805c43a9fb
naime-ahmed <naime2molla@gmail.com>
2024-10-18 02:50:50 +0600

    handled onclick to fire a modal

--- front-end/src/components/ui/button/AddBtn/AddBtn.jsx
+++ front-end/src/components/ui/button/AddBtn/AddBtn.jsx
@@ -1,10 +1,11 @@
 import styles from "./AddBtn.module.css";
 
-const AddBtn = () => {
+const AddBtn = ({ handleRequirementClick }) => {
   return (
     <button
       className={`${styles.iconBtn} ${styles.addBtn}`}
       aria-label="Start a new week"
+      onClick={handleRequirementClick}
     >
       <div className={styles.addIcon}></div>
       <div className={styles.btnTxt}>New Week</div>
