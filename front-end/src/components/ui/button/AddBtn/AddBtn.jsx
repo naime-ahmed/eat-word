02cedf3c2d40f972204bc8df5c2ad50a3df38709
naime-ahmed <naime2molla@gmail.com>
2024-10-19 02:05:53 +0600

    changed props name

--- front-end/src/components/ui/button/AddBtn/AddBtn.jsx
+++ front-end/src/components/ui/button/AddBtn/AddBtn.jsx
@@ -1,11 +1,11 @@
 import styles from "./AddBtn.module.css";
 
-const AddBtn = ({ handleRequirementClick }) => {
+const AddBtn = ({ handleOpenModal }) => {
   return (
     <button
       className={`${styles.iconBtn} ${styles.addBtn}`}
       aria-label="Start a new week"
-      onClick={handleRequirementClick}
+      onClick={handleOpenModal}
     >
       <div className={styles.addIcon}></div>
       <div className={styles.btnTxt}>New Week</div>
