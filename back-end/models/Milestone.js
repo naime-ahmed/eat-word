dc8f8606e3f6d8f3d754486e7774a206bb655fe6
naime-ahmed <naime2molla@gmail.com>
2024-10-21 02:28:32 +0600

    renamed Week to Milestone

---
+++ back-end/models/Milestone.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const milestoneSchema = new mongoose.Schema(
+  {
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "People",
+      required: true,
+    },
+    name: { type: String, required: true, trim: true },
+    targetWords: { type: Number, required: true },
+    wordsCount: { type: Number, required: true },
+    memorizedCount: { type: Number, required: true },
+    revisionCount: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+// Adding an index for querying by user
+milestoneSchema.index({ addedBy: 1 });
+
+const Milestone = mongoose.model("Milestone", milestoneSchema);
+
+export default Milestone;
