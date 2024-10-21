edcfd7a6c8587815a123596376da56c530ad02dd
naime-ahmed <naime2molla@gmail.com>
2024-10-22 01:51:17 +0600

    redesigned the my space content section to fit with different type of milestones

--- front-end/src/pages/MySpace/MySpace.jsx
+++ front-end/src/pages/MySpace/MySpace.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import TakeWeekRequirements from "../../components/popups/TakeWeekRequirements/TakeWeekRequirements";
+import TakeMilestoneRequirements from "../../components/popups/milestoneRequirements/milestoneRequirements";
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
 import AddBtn from "../../components/ui/button/AddBtn/AddBtn";
@@ -17,137 +17,119 @@ const MySpace = () => {
       <Header />
       <div className={styles.myspaceContainer}>
         <div className={styles.mySpaceHeading}>
-          <div className={styles.createNewWeekBtn}>
+          <div className={styles.createNewMilestoneBtn}>
             <AddBtn handleOpenModal={openModal} />
           </div>
-          <TakeWeekRequirements isOpen={isModalOpen} onClose={closeModal} />
+          <TakeMilestoneRequirements
+            isOpen={isModalOpen}
+            onClose={closeModal}
+          />
         </div>
         <div className={styles.mySpaceContent}>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
+          <div className={styles.sevenDayChallenge}>
+            <div className={styles.sevenDayHeading}>
+              <h3>7 Days Challenges</h3>
+            </div>
+            <div className={styles.sevenDayContent}>
+              <div className={styles.milestoneCard}>
+                <div className={styles.milestoneMainContent}>
+                  <div className={styles.milestoneNum}>
+                    <p>milestone 01</p>
+                  </div>
+                  <div className={styles.wordVerdict}>
+                    <p title="Memorized">39</p>
+                    <p title="Required revision">11</p>
+                  </div>
+                </div>
+                <p className={styles.milestoneCardLastEdit}>
+                  Last Touched: 17th oct 24
+                </p>
               </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
+              <div className={styles.milestoneCard}>
+                <div className={styles.milestoneMainContent}>
+                  <div className={styles.milestoneNum}>
+                    <p>milestone 01</p>
+                  </div>
+                  <div className={styles.wordVerdict}>
+                    <p title="Memorized">39</p>
+                    <p title="Required revision">11</p>
+                  </div>
+                </div>
+                <p className={styles.milestoneCardLastEdit}>
+                  Last Touched: 17th oct 24
+                </p>
               </div>
             </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
           </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
+          <div className={styles.threeDayChallenge}>
+            <div className={styles.threeDayHeading}>
+              <h3>3 Days Challenges</h3>
+            </div>
+            <div className={styles.threeDayContent}>
+              <div className={styles.milestoneCard}>
+                <div className={styles.milestoneMainContent}>
+                  <div className={styles.milestoneNum}>
+                    <p>milestone 01</p>
+                  </div>
+                  <div className={styles.wordVerdict}>
+                    <p title="Memorized">39</p>
+                    <p title="Required revision">11</p>
+                  </div>
+                </div>
+                <p className={styles.milestoneCardLastEdit}>
+                  Last Touched: 17th oct 24
+                </p>
               </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
+              <div className={styles.milestoneCard}>
+                <div className={styles.milestoneMainContent}>
+                  <div className={styles.milestoneNum}>
+                    <p>milestone 01</p>
+                  </div>
+                  <div className={styles.wordVerdict}>
+                    <p title="Memorized">39</p>
+                    <p title="Required revision">11</p>
+                  </div>
+                </div>
+                <p className={styles.milestoneCardLastEdit}>
+                  Last Touched: 17th oct 24
+                </p>
               </div>
             </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
           </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
+          <div className={styles.flexibleLearning}>
+            <div className={styles.flexibleLHeading}>
+              <h3>Flexible Learning</h3>
+            </div>
+            <div className={styles.flexibleLContent}>
+              <div className={styles.milestoneCard}>
+                <div className={styles.milestoneMainContent}>
+                  <div className={styles.milestoneNum}>
+                    <p>milestone 01</p>
+                  </div>
+                  <div className={styles.wordVerdict}>
+                    <p title="Memorized">39</p>
+                    <p title="Required revision">11</p>
+                  </div>
+                </div>
+                <p className={styles.milestoneCardLastEdit}>
+                  Last Touched: 17th oct 24
+                </p>
               </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
+              <div className={styles.milestoneCard}>
+                <div className={styles.milestoneMainContent}>
+                  <div className={styles.milestoneNum}>
+                    <p>milestone 01</p>
+                  </div>
+                  <div className={styles.wordVerdict}>
+                    <p title="Memorized">39</p>
+                    <p title="Required revision">11</p>
+                  </div>
+                </div>
+                <p className={styles.milestoneCardLastEdit}>
+                  Last Touched: 17th oct 24
+                </p>
               </div>
             </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
-          </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
-              </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
-              </div>
-            </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
-          </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
-              </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
-              </div>
-            </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
-          </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
-              </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
-              </div>
-            </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
-          </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
-              </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
-              </div>
-            </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
-          </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
-              </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
-              </div>
-            </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
-          </div>
-          <div className={styles.weekCard}>
-            <div className={styles.weekMainContent}>
-              <div className={styles.weekNum}>
-                <p>Week 01</p>
-              </div>
-              <div className={styles.wordVerdict}>
-                <p title="Memorized">39</p>
-                <p title="Required revision">11</p>
-              </div>
-            </div>
-            <p className={styles.weekCardLastEdit}>
-              Last Touched: 17th oct 2024
-            </p>
           </div>
         </div>
       </div>
