f37fe087bb4b666593c3829d11963552b887d46b
naime-ahmed <naime2molla@gmail.com>
2024-10-23 01:52:05 +0600

    subscribe milestoneApi reducer

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,19 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducers from "../features/auth/authSlice";
-
-import signInReducers from '../features/userSignIn/userSignInSlice';
+import signInReducers from "../features/userSignIn/userSignInSlice";
 import signUpReducers from "../features/userSignUp/userSignUpSlice";
 import { authApi } from "../services/auth";
+import { milestoneApi } from "../services/milestone";
 
 const store = configureStore({
-    reducer: {
-        signIn: signInReducers,
-        signUp: signUpReducers,
-        auth: authReducers,
-        [authApi.reducerPath]: authApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
-    devTools: import.meta.env.NODE_ENV !== 'production',
-})
+  reducer: {
+    signIn: signInReducers,
+    signUp: signUpReducers,
+    auth: authReducers,
+    [authApi.reducerPath]: authApi.reducer,
+    [milestoneApi.reducerPath]: milestoneApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(milestoneApi.middleware),
+  devTools: import.meta.env.NODE_ENV !== "production",
+});
 
 export default store;
