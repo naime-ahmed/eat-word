b4ec829b63c9668e21178c7aab4150fb3a06444b
naime-ahmed <naime2molla@gmail.com>
2024-10-23 01:59:05 +0600

    formated the code via prettier

--- front-end/src/features/userSignUp/userSignUpSlice.js
+++ front-end/src/features/userSignUp/userSignUpSlice.js
@@ -1,48 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    newUser: {
+  newUser: {
+    fName: "",
+    email: "",
+    password: "",
+    surePass: "",
+    captcha: "",
+    agree: false,
+  },
+  newUserErrors: {},
+};
+
+const signUpSlice = createSlice({
+  name: "singUp",
+  initialState,
+  reducers: {
+    // Update user fields
+    updateNewUser(state, action) {
+      const { name, value } = action.payload;
+      state.newUser[name] = value;
+    },
+
+    // set user error
+    setUserNewErrors(state, action) {
+      state.newUserErrors = action.payload;
+    },
+
+    // Reset form
+    resetNewUserForm(state) {
+      state.newUser = {
         fName: "",
         email: "",
         password: "",
         surePass: "",
         captcha: "",
         agree: false,
+      };
+      state.newUserErrors = {};
     },
-    newUserErrors : {}
-}
+  },
+});
 
-const signUpSlice = createSlice({
-    name: "singUp",
-    initialState,
-    reducers:{
-        // Update user fields
-        updateNewUser(state, action){
-            const {name, value} = action.payload;
-            state.newUser[name] = value;
-        },
-        
-        // set user error
-        setUserNewErrors(state, action){
-            state.newUserErrors = action.payload;
-        },
-
-        // Reset form
-        resetNewUserForm(state){
-            state.newUser = {
-                fName: "",
-                email: "",
-                password: "",
-                surePass: "",
-                captcha: "",
-                agree: false,
-            };
-            state.newUserErrors = {};
-        }
-    }
-})
-
-export const {updateNewUser, setUserNewErrors, resetNewUserForm } = signUpSlice.actions;
+export const { updateNewUser, setUserNewErrors, resetNewUserForm } =
+  signUpSlice.actions;
 
 // Export the reducer
 export default signUpSlice.reducer;
