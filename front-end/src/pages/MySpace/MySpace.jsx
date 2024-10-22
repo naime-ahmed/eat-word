8817369f1e277f64c0a5b26c2a221bdb23dff287
naime-ahmed <naime2molla@gmail.com>
2024-10-23 02:04:49 +0600

    fetch data from server and start redesigning the view

--- front-end/src/pages/MySpace/MySpace.jsx
+++ front-end/src/pages/MySpace/MySpace.jsx
@@ -4,10 +4,26 @@ import TakeMilestoneRequirements from "../../components/popups/milestoneRequirem
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
 import AddBtn from "../../components/ui/button/AddBtn/AddBtn";
+import { useBringMilestonesQuery } from "../../services/milestone";
 import styles from "./MySpace.module.css";
 
 const MySpace = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [viewChallenge, setViewChallenge] = useState("seven");
+
+  const { data, isLoading, isError, Error } = useBringMilestonesQuery();
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    console.log(Error);
+    return <p>something went wrong</p>;
+  }
+  console.log(data);
+  const filteredChallenges = data?.milestones?.filter(
+    (challenge) => challenge.challengeType === viewChallenge
+  );
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -26,7 +42,53 @@ const MySpace = () => {
           />
         </div>
         <div className={styles.mySpaceContent}>
-          <div className={styles.sevenDayChallenge}>
+          <div className={styles.tabs}>
+            <ul>
+              <li
+                onClick={() => setViewChallenge("seven")}
+                className={viewChallenge === "seven" ? styles.active : ""}
+              >
+                7-Day challenges
+              </li>
+              <li
+                onClick={() => setViewChallenge("three")}
+                className={viewChallenge === "three" ? styles.active : ""}
+              >
+                3-Day Challenges
+              </li>
+              <li
+                onClick={() => setViewChallenge("zero")}
+                className={viewChallenge === "zero" ? styles.active : ""}
+              >
+                Flexible Learning
+              </li>
+            </ul>
+          </div>
+          <div className="challenges">
+            {filteredChallenges.length === 0 ? (
+              <p>You have not taken challenge yet</p>
+            ) : (
+              <div>
+                {filteredChallenges.map((challenge) => (
+                  <p key={challenge._id}>
+                    {challenge.name} {challenge.challengeType}
+                  </p>
+                ))}
+              </div>
+            )}
+            {}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MySpace;
+
+{
+  /* <div className={styles.sevenDayChallenge}>
             <div className={styles.sevenDayHeading}>
               <h3>7 Days Challenges</h3>
             </div>
@@ -34,7 +96,7 @@ const MySpace = () => {
               <div className={styles.milestoneCard}>
                 <div className={styles.milestoneMainContent}>
                   <div className={styles.milestoneNum}>
-                    <p>milestone 01</p>
+                    <p>milestone 00000000000000000000001</p>
                   </div>
                   <div className={styles.wordVerdict}>
                     <p title="Memorized">39</p>
@@ -130,12 +192,5 @@ const MySpace = () => {
                 </p>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
-};
-
-export default MySpace;
+          </div> */
+}
