0983d5cb3edd26d3b71ca6efaf2bae0adbd5e12a
naime-ahmed <naime2molla@gmail.com>
2024-10-24 02:46:59 +0600

    showed content dynamically and handlled errors

--- front-end/src/pages/MySpace/MySpace.jsx
+++ front-end/src/pages/MySpace/MySpace.jsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 
 import TakeMilestoneRequirements from "../../components/popups/milestoneRequirements/milestoneRequirements";
+import Error from "../../components/shared/Error/Error";
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
 import AddBtn from "../../components/ui/button/AddBtn/AddBtn";
 import { useBringMilestonesQuery } from "../../services/milestone";
+import { formatTimeAgo } from "../../utils/formateTimeAgo";
 import styles from "./MySpace.module.css";
 
 const MySpace = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [viewChallenge, setViewChallenge] = useState("seven");
+  const [viewMilestone, setViewMilestone] = useState("seven");
 
-  const { data, isLoading, isError, Error } = useBringMilestonesQuery();
+  const { data, isLoading, isError, error } = useBringMilestonesQuery();
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
   if (isError) {
-    console.log(Error);
-    return <p>something went wrong</p>;
+    console.log("error", error);
   }
   console.log(data);
-  const filteredChallenges = data?.milestones?.filter(
-    (challenge) => challenge.challengeType === viewChallenge
+  const filteredMilestones = data?.milestones?.filter(
+    (milestone) => milestone?.milestoneType === viewMilestone
   );
 
   const openModal = () => setIsModalOpen(true);
@@ -41,44 +42,70 @@ const MySpace = () => {
             onClose={closeModal}
           />
         </div>
-        <div className={styles.mySpaceContent}>
-          <div className={styles.tabs}>
-            <ul>
-              <li
-                onClick={() => setViewChallenge("seven")}
-                className={viewChallenge === "seven" ? styles.active : ""}
-              >
-                7-Day challenges
-              </li>
-              <li
-                onClick={() => setViewChallenge("three")}
-                className={viewChallenge === "three" ? styles.active : ""}
-              >
-                3-Day Challenges
-              </li>
-              <li
-                onClick={() => setViewChallenge("zero")}
-                className={viewChallenge === "zero" ? styles.active : ""}
-              >
-                Flexible Learning
-              </li>
-            </ul>
+
+        {isError ? (
+          <Error error={error}></Error>
+        ) : (
+          <div className={styles.mySpaceContent}>
+            <div className={styles.tabs}>
+              <ul>
+                <li
+                  onClick={() => setViewMilestone("seven")}
+                  className={viewMilestone === "seven" ? styles.active : ""}
+                >
+                  7-Day challenges
+                </li>
+                <li
+                  onClick={() => setViewMilestone("three")}
+                  className={viewMilestone === "three" ? styles.active : ""}
+                >
+                  3-Day Challenges
+                </li>
+                <li
+                  onClick={() => setViewMilestone("zero")}
+                  className={viewMilestone === "zero" ? styles.active : ""}
+                >
+                  Flexible Learning
+                </li>
+              </ul>
+            </div>
+            <div className={styles.milestonesSection}>
+              {filteredMilestones?.length === 0 ? (
+                <div>
+                  <Error
+                    error={{
+                      data: {
+                        message: "You've not taken this challenge yet!",
+                      },
+                      status: 404,
+                    }}
+                  />
+                </div>
+              ) : (
+                <div className={styles.milestones}>
+                  {filteredMilestones?.map((milestone) => (
+                    <div key={milestone._id} className={styles.milestoneCard}>
+                      <div className={styles.milestoneMainContent}>
+                        <div className={styles.milestoneNum}>
+                          <p>{milestone.name}</p>
+                        </div>
+                        <div className={styles.wordVerdict}>
+                          <p title="Memorized">{milestone.memorizedCount}</p>
+                          <p title="Required revision">
+                            {milestone.revisionCount}
+                          </p>
+                        </div>
+                      </div>
+                      <p className={styles.milestoneCardLastEdit}>
+                        Edited: {formatTimeAgo(milestone.updatedAt)}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
-          <div className="challenges">
-            {filteredChallenges.length === 0 ? (
-              <p>You have not taken challenge yet</p>
-            ) : (
-              <div>
-                {filteredChallenges.map((challenge) => (
-                  <p key={challenge._id}>
-                    {challenge.name} {challenge.challengeType}
-                  </p>
-                ))}
-              </div>
-            )}
-            {}
-          </div>
-        </div>
+        )}
       </div>
       <Footer />
     </div>
@@ -86,111 +113,3 @@ const MySpace = () => {
 };
 
 export default MySpace;
-
-{
-  /* <div className={styles.sevenDayChallenge}>
-            <div className={styles.sevenDayHeading}>
-              <h3>7 Days Challenges</h3>
-            </div>
-            <div className={styles.sevenDayContent}>
-              <div className={styles.milestoneCard}>
-                <div className={styles.milestoneMainContent}>
-                  <div className={styles.milestoneNum}>
-                    <p>milestone 00000000000000000000001</p>
-                  </div>
-                  <div className={styles.wordVerdict}>
-                    <p title="Memorized">39</p>
-                    <p title="Required revision">11</p>
-                  </div>
-                </div>
-                <p className={styles.milestoneCardLastEdit}>
-                  Last Touched: 17th oct 24
-                </p>
-              </div>
-              <div className={styles.milestoneCard}>
-                <div className={styles.milestoneMainContent}>
-                  <div className={styles.milestoneNum}>
-                    <p>milestone 01</p>
-                  </div>
-                  <div className={styles.wordVerdict}>
-                    <p title="Memorized">39</p>
-                    <p title="Required revision">11</p>
-                  </div>
-                </div>
-                <p className={styles.milestoneCardLastEdit}>
-                  Last Touched: 17th oct 24
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.threeDayChallenge}>
-            <div className={styles.threeDayHeading}>
-              <h3>3 Days Challenges</h3>
-            </div>
-            <div className={styles.threeDayContent}>
-              <div className={styles.milestoneCard}>
-                <div className={styles.milestoneMainContent}>
-                  <div className={styles.milestoneNum}>
-                    <p>milestone 01</p>
-                  </div>
-                  <div className={styles.wordVerdict}>
-                    <p title="Memorized">39</p>
-                    <p title="Required revision">11</p>
-                  </div>
-                </div>
-                <p className={styles.milestoneCardLastEdit}>
-                  Last Touched: 17th oct 24
-                </p>
-              </div>
-              <div className={styles.milestoneCard}>
-                <div className={styles.milestoneMainContent}>
-                  <div className={styles.milestoneNum}>
-                    <p>milestone 01</p>
-                  </div>
-                  <div className={styles.wordVerdict}>
-                    <p title="Memorized">39</p>
-                    <p title="Required revision">11</p>
-                  </div>
-                </div>
-                <p className={styles.milestoneCardLastEdit}>
-                  Last Touched: 17th oct 24
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.flexibleLearning}>
-            <div className={styles.flexibleLHeading}>
-              <h3>Flexible Learning</h3>
-            </div>
-            <div className={styles.flexibleLContent}>
-              <div className={styles.milestoneCard}>
-                <div className={styles.milestoneMainContent}>
-                  <div className={styles.milestoneNum}>
-                    <p>milestone 01</p>
-                  </div>
-                  <div className={styles.wordVerdict}>
-                    <p title="Memorized">39</p>
-                    <p title="Required revision">11</p>
-                  </div>
-                </div>
-                <p className={styles.milestoneCardLastEdit}>
-                  Last Touched: 17th oct 24
-                </p>
-              </div>
-              <div className={styles.milestoneCard}>
-                <div className={styles.milestoneMainContent}>
-                  <div className={styles.milestoneNum}>
-                    <p>milestone 01</p>
-                  </div>
-                  <div className={styles.wordVerdict}>
-                    <p title="Memorized">39</p>
-                    <p title="Required revision">11</p>
-                  </div>
-                </div>
-                <p className={styles.milestoneCardLastEdit}>
-                  Last Touched: 17th oct 24
-                </p>
-              </div>
-            </div>
-          </div> */
-}
