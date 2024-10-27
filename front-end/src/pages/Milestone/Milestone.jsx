60b0147e3e0d327478754506e4bd46079669cbb1
naime-ahmed <naime2molla@gmail.com>
2024-10-28 02:02:15 +0600

    added header and footer, creating the strucuter

--- front-end/src/pages/Milestone/Milestone.jsx
+++ front-end/src/pages/Milestone/Milestone.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Footer from "../../components/shared/Footer/Footer";
+import Header from "../../components/shared/Header/Header";
 import styles from "./Milestone.module.css";
 
 const Milestone = () => {
@@ -9,8 +11,26 @@ const Milestone = () => {
 
   return (
     <div className={styles.milestonePage}>
-      Welcome to milestone {user.email} sir, milestone : {milestoneId}, you :{" "}
-      {user.id}
+      <Header />
+      <div className={styles.milestoneContent}>
+        Welcome to milestone {user.email} sir, milestone : {milestoneId}, you :{" "}
+        {user.id}
+        <div className={styles.milestoneHeading}>
+          <div>
+            <p>Name of milestone</p>
+            <p>type of challenge</p>
+          </div>
+          <div>
+            <p>Time left: </p>
+          </div>
+          <div>
+            <p>Number of targeted words</p>
+            <p>Number of current words</p>
+          </div>
+        </div>
+        <div className={styles.milestoneTable}></div>
+      </div>
+      <Footer />
     </div>
   );
 };
