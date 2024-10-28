0a553d4440f30be7b1a092a0fec9756effbf6ac4
naime-ahmed <naime2molla@gmail.com>
2024-10-29 01:38:17 +0600

    brigning specific milestone based on user prefferences

--- front-end/src/pages/Milestone/Milestone.jsx
+++ front-end/src/pages/Milestone/Milestone.jsx
@@ -1,35 +1,51 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Error from "../../components/shared/Error/Error";
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
+import { useBringMilestonesQuery } from "../../services/milestone";
 import styles from "./Milestone.module.css";
 
 const Milestone = () => {
   const { user } = useSelector((state) => state.auth);
-
   const { milestoneId } = useParams();
+
+  const { data, isLoading, isError, error } = useBringMilestonesQuery();
+
+  const curMilestone = data?.milestones?.filter(
+    (milestone) => milestone._id === milestoneId
+  );
+
+  console.log(curMilestone);
 
   return (
     <div className={styles.milestonePage}>
       <Header />
-      <div className={styles.milestoneContent}>
-        Welcome to milestone {user.email} sir, milestone : {milestoneId}, you :{" "}
-        {user.id}
-        <div className={styles.milestoneHeading}>
-          <div>
-            <p>Name of milestone</p>
-            <p>type of challenge</p>
+
+      {isLoading ? (
+        <p>loading...</p>
+      ) : isError ? (
+        <Error error={error} />
+      ) : (
+        <div className={styles.milestoneContent}>
+          Welcome to milestone {user?.email} sir, milestone : {milestoneId}, you
+          : {user?.id}
+          <div className={styles.milestoneHeading}>
+            <div>
+              <p>Challenge name:</p>
+              <p>Challenge type:</p>
+            </div>
+            <div>
+              <p>Time left: </p>
+            </div>
+            <div>
+              <p>Number of targeted words:</p>
+              <p>Number of current words:</p>
+            </div>
           </div>
-          <div>
-            <p>Time left: </p>
-          </div>
-          <div>
-            <p>Number of targeted words</p>
-            <p>Number of current words</p>
-          </div>
+          <div className={styles.milestoneTable}></div>
         </div>
-        <div className={styles.milestoneTable}></div>
-      </div>
+      )}
       <Footer />
     </div>
   );
