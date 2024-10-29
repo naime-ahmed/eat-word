97690c78ab543720fd829bd8866da5456c4f8a1c
naime-ahmed <naime2molla@gmail.com>
2024-10-30 00:56:26 +0600

    changed the structural design of the header section

--- front-end/src/pages/Milestone/Milestone.jsx
+++ front-end/src/pages/Milestone/Milestone.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Countdown from "../../components/dynamicComponents/Countdown/Countdown";
 import Error from "../../components/shared/Error/Error";
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
@@ -9,14 +11,33 @@ import styles from "./Milestone.module.css";
 const Milestone = () => {
   const { user } = useSelector((state) => state.auth);
   const { milestoneId } = useParams();
-
   const { data, isLoading, isError, error } = useBringMilestonesQuery();
+  console.log(user);
+  // State for milestone name
+  const [milestoneName, setMilestoneName] = useState("");
 
   const curMilestone = data?.milestones?.filter(
     (milestone) => milestone._id === milestoneId
-  );
+  )[0];
 
-  console.log(curMilestone);
+  let duration = 7;
+  if (curMilestone?.milestoneType === "three") {
+    duration = 3;
+  }
+
+  // Set initial milestone name when data is loaded
+  useEffect(() => {
+    if (curMilestone?.name) {
+      setMilestoneName(curMilestone.name);
+    }
+  }, [curMilestone]);
+
+  // Handle input change
+  const handleNameChange = (e) => {
+    setMilestoneName(e.target.value);
+    // Here implement throttled server update
+    // Example: throttledUpdateServer(e.target.value);
+  };
 
   return (
     <div className={styles.milestonePage}>
@@ -24,25 +45,30 @@ const Milestone = () => {
 
       {isLoading ? (
         <p>loading...</p>
-      ) : isError ? (
-        <Error error={error} />
       ) : (
         <div className={styles.milestoneContent}>
-          Welcome to milestone {user?.email} sir, milestone : {milestoneId}, you
-          : {user?.id}
-          <div className={styles.milestoneHeading}>
-            <div>
-              <p>Challenge name:</p>
-              <p>Challenge type:</p>
+          {isError ? (
+            <Error error={error} />
+          ) : (
+            <div className={styles.milestoneHeading}>
+              <div className={styles.milestoneName}>
+                <input
+                  type="text"
+                  name="milestoneName"
+                  value={milestoneName}
+                  onChange={handleNameChange}
+                  className={styles.milestoneNameInput}
+                />
+              </div>
+              <div className={styles.milestoneTimeLeft}>
+                <Countdown
+                  createdAt={curMilestone.createdAt}
+                  duration={duration}
+                />
+              </div>
             </div>
-            <div>
-              <p>Time left: </p>
-            </div>
-            <div>
-              <p>Number of targeted words:</p>
-              <p>Number of current words:</p>
-            </div>
-          </div>
+          )}
+          <div className={styles.divider}></div>
           <div className={styles.milestoneTable}></div>
         </div>
       )}
