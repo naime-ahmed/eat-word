e5b09709dc80dc2bb01dc639d3e74ac5cacd8884
naime-ahmed <naime2molla@gmail.com>
2024-11-08 02:07:30 +0600

    updated redux slice path with new one

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { checkAuthentication, setLoading } from "./features/auth/authSlice";
+import { checkAuthentication, setLoading } from "./features/authSlice";
 
 function App() {
   const dispatch = useDispatch();
