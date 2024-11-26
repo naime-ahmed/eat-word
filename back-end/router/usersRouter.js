39c653890e00f1ccbae4d24cae64bcf65d0bb8fb
naime-ahmed <naime2molla@gmail.com>
2024-11-27 00:52:12 +0600

    added route for user data update controller

--- back-end/router/usersRouter.js
+++ back-end/router/usersRouter.js
@@ -1,7 +1,8 @@
 // external imports
 import express from "express";
 import { removeUser } from "../controller/usersController/deleteController.js";
-import { getUserById } from "../controller/usersController/getUserById.js";
+import { getUserById } from "../controller/usersController/getUserController.js";
+import { updateUserById } from "../controller/usersController/updateUserController.js";
 import { verifyAccessToken } from "../middlewares/validate/verifyAccessToken.js";
 
 // internal imports
@@ -13,6 +14,9 @@ const router = express.Router();
 
 // get a user by id
 router.get("/:userId", verifyAccessToken, getUserById);
+
+// update user info by id
+router.put("/", verifyAccessToken, updateUserById);
 
 // delete user
 router.delete("/delete-me", removeUser);
