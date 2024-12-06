a5db35107597bb62ecef0003c8108032de11b3e9
naime-ahmed <naime2molla@gmail.com>
2024-12-07 02:07:13 +0600

    removed redundant code and used select method to exclude pass

--- back-end/controller/usersController/getUserController.js
+++ back-end/controller/usersController/getUserController.js
@@ -9,29 +9,17 @@ async function getUserById(req, res) {
       return res.status(400).json({ message: "Unauthorized request!" });
     }
     // Fetch user from the database
-    const user = await User.findById(userId);
+    const user = await User.findById(userId).select("-password");
 
     // Check if user exists
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    const userWithoutPass = {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      profilePicture: user.profilePicture,
-      role: user.role,
-      authProvider: user.authProvider,
-      preferredLang: user.preferredLang,
-      preferredDevice: user.preferredDevice,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
-
+    
     // Send success response
     res.status(200).json({
       message: "User retrieved successfully",
-      data: userWithoutPass,
+      data: user,
     });
   } catch (error) {
     console.error("Error while retrieving user:", error);
