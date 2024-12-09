70dd24f1280cfa668daa53740e036656cb4b26e0
naime-ahmed <naime2molla@gmail.com>
2024-12-10 03:06:38 +0600

    designed the minimal structure of main content area

--- front-end/src/pages/Milestone/Milestone.jsx
+++ front-end/src/pages/Milestone/Milestone.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import MilestoneDeadline from "../../components/dynamicComponents/MilestoneDeadline/MilestoneDeadline";
+import Word from "../../components/dynamicComponents/Word/Word";
 import Error from "../../components/shared/Error/Error";
 import Footer from "../../components/shared/Footer/Footer";
 import Header from "../../components/shared/Header/Header";
@@ -25,6 +26,7 @@ const Milestone = () => {
   if (curMilestone?.milestoneType === "three") {
     duration = 3;
   }
+  console.log("curr Milestone", curMilestone);
 
   // Set initial milestone name when data is loaded
   useEffect(() => {
@@ -51,26 +53,71 @@ const Milestone = () => {
           {isError ? (
             <Error error={error} />
           ) : (
-            <div className={styles.milestoneHeading}>
-              <div className={styles.milestoneName}>
-                <input
-                  type="text"
-                  name="milestoneName"
-                  value={milestoneName}
-                  onChange={handleNameChange}
-                  className={styles.milestoneNameInput}
-                />
+            <>
+              <div className={styles.milestoneHeading}>
+                <div className={styles.milestoneName}>
+                  <input
+                    type="text"
+                    name="milestoneName"
+                    value={milestoneName}
+                    onChange={handleNameChange}
+                    className={styles.milestoneNameInput}
+                  />
+                </div>
+                <div className={styles.milestoneTimeLeft}>
+                  <MilestoneDeadline
+                    createdAt={curMilestone.createdAt}
+                    duration={duration}
+                  />
+                </div>
               </div>
-              <div className={styles.milestoneTimeLeft}>
-                <MilestoneDeadline
-                  createdAt={curMilestone.createdAt}
-                  duration={duration}
-                />
+
+              <div className={styles.divider}></div>
+              <div className={styles.milestoneWords}>
+                <div className={styles.wordsHeading}>
+                  <div className={styles.wordColumn}>Word</div>
+                  <div className={styles.meaningColumn}>Meaning</div>
+                  {curMilestone.learnSynonyms && (
+                    <div className={styles.synonymColumn}>Synonyms</div>
+                  )}
+                  {curMilestone.includeDefinition && (
+                    <div className={styles.engDefinitionColumn}>
+                      English Definition
+                    </div>
+                  )}
+                  <div className={styles.exmInSenColumn}>
+                    Example in a sentence
+                  </div>
+                </div>
+                <div className={styles.words}>
+                  <Word
+                    hasSynonym={Boolean(curMilestone.learnSynonyms)}
+                    hasDefinition={Boolean(curMilestone.includeDefinition)}
+                  />
+                  <Word
+                    hasSynonym={Boolean(curMilestone.learnSynonyms)}
+                    hasDefinition={Boolean(curMilestone.includeDefinition)}
+                  />
+                  <Word
+                    hasSynonym={Boolean(curMilestone.learnSynonyms)}
+                    hasDefinition={Boolean(curMilestone.includeDefinition)}
+                  />
+                  <Word
+                    hasSynonym={Boolean(curMilestone.learnSynonyms)}
+                    hasDefinition={Boolean(curMilestone.includeDefinition)}
+                  />
+                  <Word
+                    hasSynonym={Boolean(curMilestone.learnSynonyms)}
+                    hasDefinition={Boolean(curMilestone.includeDefinition)}
+                  />
+                  <Word
+                    hasSynonym={Boolean(curMilestone.learnSynonyms)}
+                    hasDefinition={Boolean(curMilestone.includeDefinition)}
+                  />
+                </div>
               </div>
-            </div>
+            </>
           )}
-          <div className={styles.divider}></div>
-          <div className={styles.milestoneTable}></div>
         </div>
       )}
       <Footer />
