cfd1d412fe934b4803816059db46d89909f6c903
naime-ahmed <naime2molla@gmail.com>
2025-01-06 03:05:45 +0600

    feat(pop menu): implement api call action

--- front-end/src/components/Popup/PopUpContents/TableRowMenu/TableRowMenu.jsx
+++ front-end/src/components/Popup/PopUpContents/TableRowMenu/TableRowMenu.jsx
@@ -1,12 +1,92 @@
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
+import { RiDeleteBin4Line } from "react-icons/ri";
+import { VscCircleFilled } from "react-icons/vsc";
+import {
+  useDeleteWordMutation,
+  useEditWordMutation,
+} from "../../../../services/word";
 import styles from "./TableRowMenu.module.css";
 
-const TableRowMenu = () => {
+const TableRowMenu = ({ curWord, onClose }) => {
+  const [editWord, { isLoading, isError, error }] = useEditWordMutation();
+  const [
+    deleteWord,
+    { isLoading: deleteIsLoading, isError: deleteIsError, error: deleteError },
+  ] = useDeleteWordMutation();
+
+  console.log(curWord);
+  const handleEdit = (editedField) => {
+    try {
+      editWord({
+        wordId: curWord?._id,
+        milestoneId: curWord?.addedMilestone,
+        updates: editedField,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEditClick = async (action, payload = "") => {
+    switch (action) {
+      case "favorite":
+        handleEdit({ isFavorite: !curWord?.isFavorite });
+        break;
+      case "hard":
+        if (curWord?.difficultyLevel === "hard") {
+          handleEdit({ difficultyLevel: "notSpecified" });
+        } else {
+          handleEdit({ difficultyLevel: "hard" });
+        }
+        break;
+      case "delete":
+        try {
+          deleteWord({
+            wordId: curWord?._id,
+            milestoneId: curWord?.addedMilestone,
+          }).then(onClose());
+        } catch (error) {
+          console.log(error);
+        }
+
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={styles.rowMenuContainer}>
       <ul>
-        <li>Mark as Favorite</li>
-        <li>Mark as hard</li>
-        <li>Delete word</li>
+        <li
+          onClick={() => handleEditClick("favorite")}
+          className={styles.markAsFav}
+        >
+          {curWord.isFavorite ? (
+            <>
+              <MdFavorite data-icon="favorite" />{" "}
+              <span>Remove from Favorite</span>
+            </>
+          ) : (
+            <>
+              <MdFavoriteBorder /> <span>Mark as Favorite</span>
+            </>
+          )}
+        </li>
+        <li onClick={() => handleEditClick("hard")} className={styles.markHard}>
+          {curWord.difficultyLevel === "hard" ? (
+            <>
+              <VscCircleFilled data-icon="hard" /> Remove from hard
+            </>
+          ) : (
+            <>
+              <VscCircleFilled /> Mark as hard
+            </>
+          )}
+        </li>
+        <li onClick={() => handleEditClick("delete")}>
+          <RiDeleteBin4Line /> Delete word
+        </li>
       </ul>
     </div>
   );
