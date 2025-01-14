247b91e8999acdc8f3992959433177676ebb5da7
naime-ahmed <naime2molla@gmail.com>
2025-01-14 23:23:13 +0600

    amend(popup): degrade from standalone popup component to a content popup

--- front-end/src/components/Popup/PopUpContents/MilestoneRequirements/MilestoneRequirements.jsx
+++ front-end/src/components/Popup/PopUpContents/MilestoneRequirements/MilestoneRequirements.jsx
@@ -4,7 +4,7 @@ import { useAddMilestoneMutation } from "../../../../services/milestone.js";
 import PrimaryBtn from "../../../ui/button/PrimaryBtn/PrimaryBtn.jsx";
 import styles from "./MilestoneRequirements.module.css";
 
-const MilestoneRequirements = ({ isOpen, onClose, handleViewMilestone }) => {
+const MilestoneRequirements = ({ handleViewMilestone }) => {
   const [newWeekFormData, setNewWeekFormData] = useState({
     milestoneType: "",
     name: "",
@@ -49,115 +49,97 @@ const MilestoneRequirements = ({ isOpen, onClose, handleViewMilestone }) => {
       learnSynonyms: false,
       includeDefinition: false,
     });
-    onClose();
     handleViewMilestone(newWeekFormData.milestoneType);
     localStorage.setItem("selectedMS", newWeekFormData.milestoneType);
   };
 
-  if (!isOpen) return null;
-
   return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modalContent}>
-        <h2 className={styles.modalTitle}>Let me know your preferences</h2>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div className={styles.inputGroup}>
-            <select
-              name="milestoneType"
-              id="milestoneType"
-              value={newWeekFormData.milestoneType}
-              onChange={handleChange}
-              className={styles.inputField}
-              required
-            >
-              <option value="" disabled>
-                Select a learning plan
-              </option>
-              <option value="seven">7-Day Challenge</option>
-              <option value="three">3-Day Sprint</option>
-              <option value="zero">Flexible Learning</option>
-            </select>
-            <label htmlFor="milestoneType" className={styles.formLabel}>
-              Choose your learning plan
-            </label>
-          </div>
-          <div className={styles.inputGroup}>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={newWeekFormData.name}
-              onChange={handleChange}
-              className={styles.inputField}
-              required
-            />
-            <label htmlFor="name" className={styles.formLabel}>
-              Give it a name
-            </label>
-          </div>
-          <div className={styles.inputGroup}>
-            <input
-              type="number"
-              name="targetWords"
-              id="targetWords"
-              min="10"
-              max="100"
-              value={newWeekFormData.targetWords}
-              onChange={handleChange}
-              className={styles.inputField}
-              required
-            />
-            <label htmlFor="targetWords" className={styles.formLabel}>
-              How many words you want to learn?
-            </label>
-          </div>
-          <div className={styles.checkboxGroup}>
-            <input
-              type="checkbox"
-              name="learnSynonyms"
-              id="learnSynonyms"
-              checked={newWeekFormData.learnSynonyms}
-              onChange={handleChange}
-              className={styles.checkbox}
-            />
-            <label htmlFor="learnSynonyms" className={styles.checkboxLabel}>
-              Want to learn synonyms as well?
-            </label>
-          </div>
-          <div className={styles.checkboxGroup}>
-            <input
-              type="checkbox"
-              name="includeDefinition"
-              id="includeDefinition"
-              checked={newWeekFormData.includeDefinition}
-              onChange={handleChange}
-              className={styles.checkbox}
-            />
-            <label htmlFor="includeDefinition" className={styles.checkboxLabel}>
-              Want to include English definition?
-            </label>
-          </div>
-          {isError && <p>{error.data.message}</p>}
-          <div className={styles.buttonGroup}>
-            <PrimaryBtn
-              btnType="submit"
-              colorOne="#068200"
-              colorTwo="#00a116"
-              isLoading={isLoading}
-            >
-              Let&#39;s goo
-            </PrimaryBtn>
-            <PrimaryBtn
-              btnType="button"
-              handleClick={onClose}
-              colorOne="#b30000"
-              colorTwo="#d00000"
-            >
-              Cancel
-            </PrimaryBtn>
-          </div>
-        </form>
-      </div>
+    <div className={styles.modalContent}>
+      <h2 className={styles.modalTitle}>Let me know your preferences</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.inputGroup}>
+          <select
+            name="milestoneType"
+            id="milestoneType"
+            value={newWeekFormData.milestoneType}
+            onChange={handleChange}
+            className={styles.inputField}
+            required
+          >
+            <option value="" disabled>
+              Select a learning plan
+            </option>
+            <option value="seven">7-Day Challenge</option>
+            <option value="three">3-Day Sprint</option>
+            <option value="zero">Flexible Learning</option>
+          </select>
+          <label htmlFor="milestoneType" className={styles.formLabel}>
+            Choose your learning plan
+          </label>
+        </div>
+        <div className={styles.inputGroup}>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={newWeekFormData.name}
+            onChange={handleChange}
+            className={styles.inputField}
+            required
+          />
+          <label htmlFor="name" className={styles.formLabel}>
+            Give it a name
+          </label>
+        </div>
+        <div className={styles.inputGroup}>
+          <input
+            type="number"
+            name="targetWords"
+            id="targetWords"
+            min="10"
+            max="100"
+            value={newWeekFormData.targetWords}
+            onChange={handleChange}
+            className={styles.inputField}
+            required
+          />
+          <label htmlFor="targetWords" className={styles.formLabel}>
+            How many words you want to learn?
+          </label>
+        </div>
+        <div className={styles.checkboxGroup}>
+          <input
+            type="checkbox"
+            name="learnSynonyms"
+            id="learnSynonyms"
+            checked={newWeekFormData.learnSynonyms}
+            onChange={handleChange}
+            className={styles.checkbox}
+          />
+          <label htmlFor="learnSynonyms" className={styles.checkboxLabel}>
+            Want to learn synonyms as well?
+          </label>
+        </div>
+        <div className={styles.checkboxGroup}>
+          <input
+            type="checkbox"
+            name="includeDefinition"
+            id="includeDefinition"
+            checked={newWeekFormData.includeDefinition}
+            onChange={handleChange}
+            className={styles.checkbox}
+          />
+          <label htmlFor="includeDefinition" className={styles.checkboxLabel}>
+            Want to include English definition?
+          </label>
+        </div>
+        {isError && <p>{error.data.message}</p>}
+        <div className={styles.SubmitButton}>
+          <PrimaryBtn btnType="submit" isLoading={isLoading}>
+            Let&#39;s goo
+          </PrimaryBtn>
+        </div>
+      </form>
     </div>
   );
 };
