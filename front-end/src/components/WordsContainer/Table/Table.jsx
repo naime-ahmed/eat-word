0b5d73b5a97e2cbc2f6b91c90aa2dc51272b8ef5
naime-ahmed <naime2molla@gmail.com>
2025-01-18 03:56:15 +0600

    make the update word a hook to reuse it in slider

--- front-end/src/components/WordsContainer/Table/Table.jsx
+++ front-end/src/components/WordsContainer/Table/Table.jsx
@@ -1,11 +1,8 @@
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useBringMilestoneWordQuery } from "../../../services/milestone";
-import {
-  useAppendWordMutation,
-  useEditWordMutation,
-} from "../../../services/word";
 import Notification from "../../Notification/Notification";
+import { useUpdateWords } from "../hooks/useUpdateWords.js";
 import { calculateColumnWidths, wordSchemaForClient } from "../utils.js";
 import styles from "./Table.module.css";
 import EditableCell from "./TableCells/EditableCell";
@@ -14,17 +11,12 @@ import TableRow from "./TableRow";
 
 const WordsContainer = ({ curMilestone }) => {
   const [words, setWords] = useState([]);
-  const [missingError, setMissingError] = useState("");
-  const [doNotify, setDoNotify] = useState(false);
-  const newWordsSet = useRef(new Set());
   const [rowHeights, setRowHeights] = useState([]);
+  const { updateWords, missingError, doNotify, setDoNotify } = useUpdateWords();
 
   const { data, isLoading, isError, error } = useBringMilestoneWordQuery(
     curMilestone?._id
   );
-
-  const [appendWord] = useAppendWordMutation();
-  const [editWord] = useEditWordMutation();
 
   // Effect to update words when data is available
   useEffect(() => {
@@ -90,97 +82,10 @@ const WordsContainer = ({ curMilestone }) => {
     setRowHeights((prev) => [...prev, {}]);
   };
 
-  // Update word
-  const handleEditWord = async (wordId, editedFields) => {
-    try {
-      const res = await editWord({
-        wordId,
-        milestoneId: curMilestone?._id,
-        updates: editedFields,
-      }).unwrap();
-      console.log("updated Word from server:", res);
-    } catch (error) {
-      console.log("Error while updating word", error);
-    }
+  const handleUpdate = (rowIndex, columnId, value) => {
+    updateWords(setWords, rowIndex, columnId, value, curMilestone?._id);
   };
 
-  const updateWords = (rowIndex, columnId, value) => {
-    console.log("on Update", rowIndex, columnId, value);
-
-    // Early return if inputs are invalid
-    if (
-      typeof rowIndex !== "number" ||
-      rowIndex < 0 ||
-      !columnId ||
-      value === undefined ||
-      value === null
-    ) {
-      console.error("Invalid inputs for updateWords:", {
-        rowIndex,
-        columnId,
-        value,
-      });
-      return;
-    }
-
-    setWords((prev) => {
-      // Ensure `prev` is defined and is an array
-      if (!Array.isArray(prev)) {
-        console.error("Invalid `words` state:", prev);
-        return prev || [];
-      }
-
-      // Create a copy of the previous state
-      const updatedWords = prev.map((row, index) =>
-        index === rowIndex ? { ...row, [columnId]: value } : row
-      );
-
-      // Get the word to update
-      const wordToUpdate = updatedWords[rowIndex];
-
-      // If the word is not found, return the previous state
-      if (!wordToUpdate) {
-        console.error("Word not found at index:", rowIndex);
-        return prev;
-      }
-
-      // If the word is new (no _id), append it to the server
-      if (!wordToUpdate._id) {
-        if (wordToUpdate.word === "") {
-          setMissingError("word is required! fill the word.");
-          setDoNotify(true);
-          return prev;
-        }
-
-        // Prevent duplicate append calls
-        if (newWordsSet.current.has(rowIndex)) return prev;
-        newWordsSet.current.add(rowIndex);
-
-        appendWord(wordToUpdate)
-          .unwrap()
-          .then((response) => {
-            setWords((prevWords) =>
-              prevWords.map((word, index) =>
-                index === rowIndex ? response?.newWord : word
-              )
-            );
-          })
-          .catch((error) => {
-            console.error("Error while appending word:", error);
-            setDoNotify(true);
-            setMissingError("something went wrong while updating word");
-            newWordsSet.current.delete(rowIndex);
-          });
-      } else {
-        // If the word exists, update it on the server
-        handleEditWord(wordToUpdate._id, { [columnId]: value });
-      }
-
-      return updatedWords;
-    });
-  };
-
-  // Column sizes in pixels
   const { wordSize, meaningSize, synonymsSize, definitionsSize, examplesSize } =
     calculateColumnWidths(curMilestone);
 
@@ -242,7 +147,7 @@ const WordsContainer = ({ curMilestone }) => {
     data: words,
     getCoreRowModel: getCoreRowModel(),
     meta: {
-      updateWords,
+      updateWords: handleUpdate,
       updateRowHeight,
       rowHeights,
     },
