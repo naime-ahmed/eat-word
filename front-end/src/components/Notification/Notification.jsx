68fdddaff184b96f7730e86fa2891c77c022ab54
naime-ahmed <naime2molla@gmail.com>
2025-01-20 04:14:11 +0600

    use react protal for better management and choosing icon based on message type

--- front-end/src/components/Notification/Notification.jsx
+++ front-end/src/components/Notification/Notification.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
-import { MdOutlineDone } from "react-icons/md";
+import { createPortal } from "react-dom";
+import { BiMessageAltError } from "react-icons/bi";
+import { LuCircleCheckBig, LuMessageSquareWarning } from "react-icons/lu";
 import styles from "./Notification.module.css";
 
-const Notification = ({ title, message, isOpen, onClose }) => {
+const Notification = ({
+  title,
+  message,
+  iconType = "warning",
+  isOpen,
+  onClose,
+  duration = 5000,
+}) => {
   const [visible, setVisible] = useState(isOpen);
-
+  console.log("icon type", iconType);
+  console.log("title", title);
   useEffect(() => {
     let timeout;
 
@@ -12,30 +22,36 @@ const Notification = ({ title, message, isOpen, onClose }) => {
       timeout = setTimeout(() => {
         setVisible(false);
         onClose();
-      }, 3000); // Close after 3 seconds
+      }, duration);
     }
 
     return () => {
       clearTimeout(timeout);
     };
-  }, [visible, onClose]);
+  }, [visible, onClose, duration]);
 
   useEffect(() => {
     setVisible(isOpen);
   }, [isOpen]);
+  const icons = {
+    error: <BiMessageAltError className={styles.error} />,
+    warning: <LuMessageSquareWarning className={styles.warning} />,
+    success: <LuCircleCheckBig className={styles.success} />,
+  };
 
   if (!visible) return null;
 
-  return (
+  return createPortal(
     <div className={styles.notification}>
       <div className={styles.content}>
-        <MdOutlineDone className={styles.icon} />
+        {icons[iconType] && icons[iconType]}
         <div>
           <h3 className={styles.title}>{title}</h3>
           <p className={styles.message}>{message}</p>
         </div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
