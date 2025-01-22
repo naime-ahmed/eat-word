072062c17920e521df57086e5f43fb93c36e094a
naime-ahmed <naime2molla@gmail.com>
2025-01-23 03:22:03 +0600

    updating user data state on app load

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,21 @@
+// src/App.js
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { checkAuthentication, setLoading } from "./features/authSlice";
+import { setLoading } from "./features/authSlice";
+import { setUserData, setUserError } from "./features/userSlice.js";
+import { useBringUserByIdQuery } from "./services/user.js";
+import { checkAuthentication } from "./thunks/authThunk.js";
 
 function App() {
   const dispatch = useDispatch();
+  const authUser = useSelector((state) => state.auth.user);
+
+  // fetch user data
+  const { data: userData, error: userError } = useBringUserByIdQuery(
+    authUser?.id,
+    { skip: !authUser?.id }
+  );
 
   useEffect(() => {
     // get the token from local storage
@@ -12,18 +23,29 @@ function App() {
 
     const verifyToken = async () => {
       if (token) {
-        await dispatch(checkAuthentication(token));
+        try {
+          await dispatch(checkAuthentication(token));
+        } catch (err) {
+          console.error("Authentication check failed", err);
+        }
       } else {
         // If no token, ensure loading is set to false
         dispatch(setLoading(false));
       }
     };
 
-    verifyToken().catch((err) => {
-      console.error("Authentication check failed", err);
-      dispatch(setLoading(false));
-    });
+    verifyToken();
   }, [dispatch]);
+
+  useEffect(() => {
+    if (userData?.data) {
+      dispatch(setUserData(userData?.data));
+    }
+
+    if (userError) {
+      dispatch(setUserError(userError));
+    }
+  }, [userData, userError, dispatch]);
 
   return <Outlet />;
 }
