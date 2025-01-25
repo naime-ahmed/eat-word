0027e35caaccc85d073114996cbbf854e88de549
naime-ahmed <naime2molla@gmail.com>
2025-01-26 03:54:34 +0600

    redesign the card

--- front-end/src/components/Testimonial/Testimonial.jsx
+++ front-end/src/components/Testimonial/Testimonial.jsx
@@ -1,44 +1,23 @@
-import { useEffect, useState } from "react";
 import styles from "./Testimonial.module.css";
 
-const Testimonial = ({ lane }) => {
-  const [testimonials, setTestimonials] = useState([]);
-  useEffect(() => {
-    fetch("/DB/testimonials.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setTestimonials(data);
-      })
-      .catch((error) => {
-        console.log("Error fetching testimonials", error);
-      });
-  }, []);
-  //   console.log(lane);
+const Testimonial = ({ name, review, profession, image }) => {
   return (
-    <div className={styles.testimonialsCardContainer}>
-      <div className={styles.testimonialsMarquee}>
-        <div className={styles[lane]}>
-          {testimonials.map((item, index) => (
-            <div key={index} className={styles.testimonialItem}>
-              <div className={styles.testimonialCard}>
-                <div className={styles.testimonialText}>{item.review}</div>
-                <div className={styles.testimonialProfile}>
-                  <img
-                    src={item.image}
-                    className={styles.profileImage}
-                    alt={item.name}
-                  />
-                  <div className={styles.profileInfo}>
-                    <div className={styles.profileName}>{item.name}</div>
-                    <div className={styles.profileTitle}>{item.profession}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+    <figure className={styles.card}>
+      <div className={styles.content}>
+        <img
+          className={styles.avatar}
+          width="32"
+          height="32"
+          alt=""
+          src={image}
+        />
+        <div>
+          <figcaption className={styles.name}>{name}</figcaption>
+          <p className={styles.username}>{profession}</p>
         </div>
       </div>
-    </div>
+      <blockquote className={styles.quote}>{review}</blockquote>
+    </figure>
   );
 };
 
