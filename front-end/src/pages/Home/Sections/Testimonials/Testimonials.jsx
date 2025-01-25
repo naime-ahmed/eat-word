a62df4a8c5cc4fa769215ad6a6af3ab58551bc20
naime-ahmed <naime2molla@gmail.com>
2025-01-26 03:48:42 +0600

    redesign the looks and added reverse animation

--- front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
+++ front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
@@ -1,9 +1,27 @@
+// Testimonials.jsx
+import { useEffect, useState } from "react";
+import Marquee from "../../../../components/Marquee/Marquee";
 import Testimonial from "../../../../components/Testimonial/Testimonial";
 import styles from "./Testimonials.module.css";
 
 const Testimonials = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  useEffect(() => {
+    fetch("/DB/testimonials.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTestimonials(data);
+      })
+      .catch((error) => {
+        console.log("Error fetching testimonials", error);
+      });
+  }, []);
+
+  const firstRow = testimonials.slice(0, testimonials.length / 2);
+  const secondRow = testimonials.slice(testimonials.length / 2);
+
   return (
-    <div className={styles.testimonialsContainer}>
+    <div className={styles.container}>
       <div className={styles.gradientLine}></div>
       <div className={styles.gradientBackground}></div>
       <div className={styles.testimonialHead}>
@@ -13,8 +31,19 @@ const Testimonials = () => {
           learner
         </p>
       </div>
-      <Testimonial lane="marqueOne" />
-      <Testimonial lane="marqueTwo" />
+
+      <Marquee pauseOnHover>
+        {firstRow.map((review) => (
+          <Testimonial key={review.username} {...review} />
+        ))}
+      </Marquee>
+      <Marquee reverse pauseOnHover>
+        {secondRow.map((review) => (
+          <Testimonial key={review.username} {...review} />
+        ))}
+      </Marquee>
+      <div className={styles.gradientLeft} />
+      <div className={styles.gradientRight} />
     </div>
   );
 };
