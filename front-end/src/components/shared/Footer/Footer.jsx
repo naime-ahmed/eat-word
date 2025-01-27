67b46d8509c1fddc3855cfddbf8b21d07bf2be50
naime-ahmed <naime2molla@gmail.com>
2025-01-28 04:21:18 +0600

    restructure the elements and tweak the theme

--- front-end/src/components/shared/Footer/Footer.jsx
+++ front-end/src/components/shared/Footer/Footer.jsx
@@ -1,62 +1,47 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
 import style from "./Footer.module.css";
 
 const Footer = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Function to update state based on window size
-  const handleResize = () => {
-    setIsMobile(window.innerWidth < 600);
-  };
-
-  // Set up resize event listener
-  useEffect(() => {
-    handleResize(); // Check initial size on mount
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize); // Clean up listener
-    };
-  }, []);
-
   return (
-    <div className={style.footerContainer}>
+    <footer className={style.footerContainer}>
+      <div className={style.footerGlow}></div>
       <div className={style.footerContentContainer}>
         <div className={style.footerLeft}>
           <div className={style.footerBrand}>
-            <h1>Eat Word</h1>
+            <img src={logo} alt="Eat Word logo" />
             <p>Elevate Your Language</p>
           </div>
-          {!isMobile && (
-            <div className={style.copyrightText}>
-              <p>© 2024 Eat Word. All Rights Reserved.</p>
-            </div>
-          )}
+          <div className={style.desktopCopyright}>
+            <p>© 2024 Eat Word. All Rights Reserved.</p>
+          </div>
         </div>
+
         <div className={style.footerRight}>
           <div className={style.footerSocials}>
-            <Link>
-              <Link to="/" className="fa-brands fa-x-twitter"></Link>
-            </Link>
-            <Link>
-              <Link to="/" className="fa-brands fa-linkedin"></Link>
-            </Link>
+            <Link
+              to="/"
+              aria-label="Twitter"
+              className="fa-brands fa-x-twitter"
+            />
+            <Link
+              to="/"
+              aria-label="LinkedIn"
+              className="fa-brands fa-linkedin"
+            />
           </div>
-          <div className={style.footerImportantLinks}>
+
+          <nav className={style.footerImportantLinks}>
             <Link to="/contact">Contact Us</Link>
             <Link to="/privacyPolicy">Privacy Policy</Link>
+          </nav>
+
+          <div className={style.mobileCopyright}>
+            <p>© 2024 Eat Word. All Rights Reserved.</p>
           </div>
-          {isMobile && (
-            <div className={style.copyrightText}>
-              <p>© 2024 Eat Word. All Rights Reserved.</p>
-            </div>
-          )}
         </div>
       </div>
-
-      <div className={style.footerGlow}></div>
-    </div>
+    </footer>
   );
 };
 
