9b986ee212e2d262533da00a7f07aff9e5135677
naime-ahmed <naime2molla@gmail.com>
2025-02-04 23:35:35 +0600

    add the intro video section below hero section

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -3,14 +3,15 @@ import Header from "../../components/shared/Header/Header";
 import style from "./Home.module.css";
 import BaseTheory from "./Sections/BaseTheory/BaseTheory";
 import Hero from "./Sections/Hero/Hero";
+import IntroVideo from "./Sections/IntroVideo/IntroVideo";
 import JourneyOfAWord from "./Sections/JourneyOfAWord/JourneyOfAWord";
 import Testimonials from "./Sections/Testimonials/Testimonials";
-
 const Home = () => {
   return (
     <div className={style.homeContainer}>
       <Header />
       <Hero />
+      <IntroVideo />
       <JourneyOfAWord />
       <BaseTheory />
       <Testimonials />
