3565a87daa3667b0afc6d875f0112ec8009bef4e
naime-ahmed <naime2molla@gmail.com>
2025-02-09 03:39:10 +0600

    relocate from dynamicComponent folder

---
+++ front-end/src/components/ActivateACCCard/ActivateACCCard.jsx
@@ -0,0 +1,52 @@
+import PrimaryBtn from "../ui/button/PrimaryBtn/PrimaryBtn";
+import styles from "./ActivateACCCard.module.css";
+
+const ActivateACCCard = ({
+  isSuccess,
+  p1,
+  p2,
+  btnText,
+  btnClick,
+  btnColorLeft,
+  btnColorRight,
+}) => {
+  return (
+    <div
+      className={styles.ActivateACCCard}
+      style={
+        isSuccess
+          ? { border: "2px solid rgb(1, 160, 35)" }
+          : { border: "2px solid rgb(153, 0, 26)" }
+      }
+    >
+      <small
+        className={isSuccess ? `${styles.successIcon}` : `${styles.errorIcon}`}
+      >
+        {isSuccess ? (
+          <i
+            className="fa-solid fa-circle-check"
+            style={{ color: "green" }}
+          ></i>
+        ) : (
+          <i
+            className="fa-solid fa-circle-exclamation"
+            style={{ color: "red" }}
+          ></i>
+        )}
+      </small>
+      <div className={styles.ActivateACCCardText}>
+        <p>{p1}</p>
+        <p>{p2}</p>
+      </div>
+      <PrimaryBtn
+        handleClick={btnClick}
+        colorOne={btnColorLeft}
+        colorTwo={btnColorRight}
+      >
+        {btnText}
+      </PrimaryBtn>
+    </div>
+  );
+};
+
+export default ActivateACCCard;
