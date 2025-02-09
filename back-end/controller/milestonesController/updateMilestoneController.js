3946c68bb5619fdcfe18ac9fe55bea420c23ebf5
naime-ahmed <naime2molla@gmail.com>
2025-02-10 04:20:46 +0600

    include the lastRecalled filed in editable list

--- back-end/controller/milestonesController/updateMilestoneController.js
+++ back-end/controller/milestonesController/updateMilestoneController.js
@@ -11,7 +11,7 @@ async function updateMilestone(req, res) {
       return res.status(400).json({ message: "Invalid milestone ID format" });
     }
 
-    const allowedFields = ['name', 'targetWords', 'wordsCount', 'memorizedCount', "revisionCount", "pinned"];
+    const allowedFields = ['name', 'targetWords', 'wordsCount', 'memorizedCount', "revisionCount", "pinned", "lastRecalled"];
     const invalidFields = Object.keys(updateData).filter(
       field => !allowedFields.includes(field)
     );
