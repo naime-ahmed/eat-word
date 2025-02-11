d462ff0e1a3dd56b4bee853a3dec12bcc614746f
naime-ahmed <naime2molla@gmail.com>
2025-02-12 03:45:30 +0600

    add client site memoization to testimonial data for a session

--- front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
+++ front-end/src/pages/Home/Sections/Testimonials/Testimonials.jsx
@@ -6,29 +6,46 @@ import styles from "./Testimonials.module.css";
 
 const Testimonials = () => {
   const [testimonials, setTestimonials] = useState([]);
+
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_EAT_WORD_BACKEND_URL}/testimonial`)
-      .then((response) => response.json())
-      .then((data) => {
-        setTestimonials(data?.testimonials);
-      })
-      .catch((error) => {
-        console.log("Error fetching testimonials", error);
-      });
+    const fetchTestimonials = () => {
+      fetch(`${import.meta.env.VITE_EAT_WORD_BACKEND_URL}/testimonial`)
+        .then((response) => response.json())
+        .then((data) => {
+          const testimonialsData = data?.testimonials || [];
+          setTestimonials(testimonialsData);
+          sessionStorage.setItem(
+            "testimonials",
+            JSON.stringify(testimonialsData)
+          );
+        })
+        .catch(console.error);
+    };
+
+    try {
+      const cached = sessionStorage.getItem("testimonials");
+      if (cached) {
+        setTestimonials(JSON.parse(cached));
+      } else {
+        fetchTestimonials();
+      }
+    } catch (error) {
+      console.error("Cache error:", error);
+      fetchTestimonials();
+    }
   }, []);
 
-  console.log(testimonials);
   const firstRow = testimonials?.slice(0, testimonials.length / 2);
   const secondRow = testimonials?.slice(testimonials.length / 2);
 
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <div className={styles.gradientLine}></div>
       <div className={styles.gradientBackground}></div>
       <div className={styles.testimonialHead}>
         <h1>Words of Praise</h1>
         <p>
-          See how &#34;Your Word&#34; is transforming vocabulary mastery for
+          See how &#34;Eat Word&#34; is transforming vocabulary mastery for
           learner
         </p>
       </div>
@@ -45,7 +62,7 @@ const Testimonials = () => {
       </Marquee>
       <div className={styles.gradientLeft} />
       <div className={styles.gradientRight} />
-    </div>
+    </section>
   );
 };
 
