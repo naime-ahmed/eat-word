7366f9f9933151ea64e7db1b3c67097cee1b081a
naime-ahmed <naime2molla@gmail.com>
2025-02-16 00:36:39 +0600

    rewrite following simple, accessible, moduler way

--- front-end/src/pages/Home/Sections/JourneyOfAWord/JourneyOfAWord.jsx
+++ front-end/src/pages/Home/Sections/JourneyOfAWord/JourneyOfAWord.jsx
@@ -1,9 +1,4 @@
-import imgForLongMemo from "../../../../assets/longTimeMemoCardIcon.png";
-import imgForRevised from "../../../../assets/revisionWeekCardIcon.png";
-import imgForShortMemo from "../../../../assets/shortTimeMemoCardIcon.png";
-import imgForTestOnSelf from "../../../../assets/testOnSelfCardIcon.png";
-import imgForListWord from "../../../../assets/wordListDownCardIcon.png";
-import imgForFindWord from "../../../../assets/wordSearchCardIcon.png";
+import { cards, dashBorderConfigs } from "../../utils.js";
 import style from "./JourneyOfAWord.module.css";
 
 const JourneyOfAWord = () => {
@@ -21,154 +16,38 @@ const JourneyOfAWord = () => {
         </p>
       </div>
       <div className={style.JourneyOfAWordPath}>
-        <div className={style.card0}>
-          <div>
-            <img
-              src={imgForFindWord}
-              alt="Image of word search"
-              width="80px"
-              height="100%"
-            />
+        {cards().map((card, index) => (
+          <div key={index} className={style[`card${index}`]}>
+            <div>
+              <img src={card.img} alt={card.alt} width="80px" height="100%" />
+            </div>
+            <div>{card.text}</div>
           </div>
-          <div>Clashed with unknown word?</div>
-        </div>
-        <div className={style.card1}>
-          <div>
-            <img
-              src={imgForListWord}
-              alt="image of word list"
-              width="80px"
-              height="100%"
-            />
+        ))}
+
+        {dashBorderConfigs().map((config, index) => (
+          <div key={index} className={style[`dashBorder${index}`]}>
+            <svg
+              viewBox={config.viewBox}
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <linearGradient
+                  id={config.id}
+                  x1="0%"
+                  y1="0%"
+                  x2="100%"
+                  y2="0%"
+                >
+                  <stop offset="0%" stopColor={config.pathColor1} />
+                  <stop offset="100%" stopColor={config.pathColor2} />{" "}
+                </linearGradient>
+              </defs>
+              <path d={config.d} className={style[`path${index}`]}></path>
+            </svg>
           </div>
-          <div>List it down and learn</div>
-        </div>
-        <div className={style.card2}>
-          <div>
-            <img
-              src={imgForShortMemo}
-              alt="image of short time memory"
-              width="80px"
-              height="100%"
-            />
-          </div>
-          <div>Now it&#39;s living in your short time memory</div>
-        </div>
-        <div className={style.card3}>
-          <div>
-            <img
-              src={imgForRevised}
-              alt="image of revise the word for a week"
-              width="80px"
-              height="100%"
-            />
-          </div>
-          <div>
-            Revise either once per day or once every two days for a week.
-          </div>
-        </div>
-        <div className={style.card4}>
-          <div>
-            <img
-              src={imgForTestOnSelf}
-              alt="image of test on self"
-              width="80px"
-              height="100%"
-            />
-          </div>
-          <div>
-            Each week, test yourself on all the words you&#39;ve learned. Mark
-            any forgotten words in red, then start next week mission.
-          </div>
-        </div>
-        <div className={style.card5}>
-          <div>
-            <img
-              src={imgForLongMemo}
-              alt="image of long time memory"
-              width="80px"
-              height="100%"
-            />
-          </div>
-          <div>
-            Successfully remembering words in your self-test indicates
-            they&#39;re now part of your long-term memory.
-          </div>
-        </div>
-        <div className={style.dashBorder0}>
-          <svg
-            width="0"
-            height="0"
-            viewBox="0 0 0 0"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.72656 1.86328C1.72656 100.386 81.5642 180.193 180.125 180.193"
-              className={style.path0}
-            ></path>
-          </svg>
-        </div>
-        <div className={style.dashBorder1}>
-          <svg
-            viewBox="0 0 182 182"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.72656 1.86328C1.72656 100.386 81.5642 180.193 180.125 180.193"
-              className={style.path1}
-            ></path>
-          </svg>
-        </div>
-        <div className={style.dashBorder2}>
-          <svg
-            viewBox="0 0 182 181"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M180.273 1.42188C180.273 99.9447 100.436 179.752 1.875 179.752"
-              className={style.path2}
-            ></path>
-          </svg>
-        </div>
-        <div className={style.dashBorder3}>
-          <svg
-            viewBox="0 0 182 182"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.72656 1.86328C1.72656 100.386 81.5642 180.193 180.125 180.193"
-              className={style.path3}
-            ></path>
-          </svg>
-        </div>
-        <div className={style.dashBorder4}>
-          <svg
-            viewBox="0 0 182 181"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M180.273 1.42188C180.273 99.9447 100.436 179.752 1.875 179.752"
-              className={style.path4}
-            ></path>
-          </svg>
-        </div>
-        <div className={style.dashBorder5}>
-          <svg
-            viewBox="0 0 182 182"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.72656 1.86328C1.72656 100.386 81.5642 180.193 180.125 180.193"
-              className={style.path5}
-            ></path>
-          </svg>
-        </div>
+        ))}
       </div>
     </section>
   );
