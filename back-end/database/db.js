95a17e5dd0fa29b9c67a7c39b1b2ab46c6348d58
naime-ahmed <naime2molla@gmail.com>
2025-02-26 03:04:29 +0600

    fix: correct the mongo atlas connection string

--- back-end/database/db.js
+++ back-end/database/db.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const connectToDb = async () => {
   let mongoConnectionString;
 
-  if (process.env.NODE_ENV === "production") {
-    // Atlas connection (production)
-    mongoConnectionString = `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASS}@eatword-disk.ijsua.mongodb.net/eatWord?retryWrites=true&w=majority`;
+  if (process.env.NODE_ENV === "development") {
+    mongoConnectionString = `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASS}@eatword-disk.ijsua.mongodb.net/eatWord?retryWrites=true&w=majority&appName=eatword-disk`;
   } else {
-    // Local MongoDB (development)
     mongoConnectionString = "mongodb://localhost:27017/eatWord";
   }
 
