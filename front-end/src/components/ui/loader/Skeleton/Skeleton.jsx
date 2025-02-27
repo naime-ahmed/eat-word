bd0e76244b3007c3710bfb976179ac6aa0a5966c
naime-ahmed <naime2molla@gmail.com>
2025-02-28 02:21:58 +0600

    refactor: redesign the code structure and receive label for loading

--- front-end/src/components/ui/loader/Skeleton/Skeleton.jsx
+++ front-end/src/components/ui/loader/Skeleton/Skeleton.jsx
@@ -1,17 +1,32 @@
+import PropTypes from "prop-types";
 import styles from "./Skeleton.module.css";
 
-const Skeleton = ({ width, height }) => {
+const Skeleton = ({ width, height, label = "" }) => {
+  const getDimension = (value) => {
+    if (typeof value === "number") return `${value}px`;
+    if (typeof value === "string" && !isNaN(value)) return `${value}px`;
+    return value;
+  };
+
   return (
     <div
       className={styles.skeleton}
       style={{
-        width: width,
-        height: height,
-        maxWidth: width,
-        maxHeight: height,
+        width: getDimension(width),
+        height: getDimension(height),
+        maxWidth: getDimension(width),
+        maxHeight: getDimension(height),
       }}
-    ></div>
+    >
+      {label}
+    </div>
   );
+};
+
+Skeleton.propTypes = {
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  label: PropTypes.string,
 };
 
 export default Skeleton;
