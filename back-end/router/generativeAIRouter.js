1f3a5a714f58733b6c6ebdcf7a6bd6fe9cfc8c19
naime-ahmed <naime2molla@gmail.com>
2025-03-12 23:48:18 +0600

    feat: add the generate milestone story path pointing to the controller

--- back-end/router/generativeAIRouter.js
+++ back-end/router/generativeAIRouter.js
@@ -2,11 +2,14 @@
 import express from "express";
 
 // internal imports
+import { generateMilestoneStory } from "../controller/generativeAIController/storyWithWordsController.js";
 import { generateWordInfo } from "../controller/generativeAIController/wordInfoController.js";
 import { verifyAccessToken } from "../middlewares/validate/verifyAccessToken.js";
 
 const router = express.Router();
 
 router.patch("/word-info/:wordId", verifyAccessToken, generateWordInfo);
+
+router.patch("/story/:milestoneId", verifyAccessToken, generateMilestoneStory);
 
 export default router;
