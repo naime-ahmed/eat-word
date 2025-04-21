b23dbb66cae8fc3cbcbf2ae31c8c3af9205c9e7f
naime-ahmed <naime2molla@gmail.com>
2025-04-21 23:05:33 +0600

    chore: apply wordInfoGenLimit middleware to word info generation route

--- back-end/router/generativeAIRouter.js
+++ back-end/router/generativeAIRouter.js
@@ -4,11 +4,12 @@ import express from "express";
 // internal imports
 import { generateMilestoneStory } from "../controller/generativeAIController/storyWithWordsController.js";
 import { generateWordInfo } from "../controller/generativeAIController/wordInfoController.js";
+import { wordInfoGenLimit } from "../middlewares/rateLimiter/wordInfoGenLimit.js";
 import { verifyAccessToken } from "../middlewares/validate/verifyAccessToken.js";
 
 const router = express.Router();
 
-router.patch("/word-info/:wordId", verifyAccessToken, generateWordInfo);
+router.patch("/word-info/:wordId", verifyAccessToken, wordInfoGenLimit,  generateWordInfo);
 
 router.patch("/story/:milestoneId", verifyAccessToken, generateMilestoneStory);
 
