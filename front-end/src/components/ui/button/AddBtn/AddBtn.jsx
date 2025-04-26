f29061ca8612592196c226168f91e222abdf2203
naime-ahmed <naime2molla@gmail.com>
2025-04-26 23:26:37 +0600

    refactor: redesign the add button

--- front-end/src/components/ui/button/AddBtn/AddBtn.jsx
+++ front-end/src/components/ui/button/AddBtn/AddBtn.jsx
@@ -1,16 +1,21 @@
+import PropTypes from "prop-types";
+import { FiPlus } from "react-icons/fi";
 import styles from "./AddBtn.module.css";
 
-const AddBtn = ({ handleOpenModal }) => {
+const AddBtn = ({ children, handleOpenModal }) => {
   return (
-    <button
-      className={`${styles.iconBtn} ${styles.addBtn}`}
-      aria-label="Start a new week"
-      onClick={handleOpenModal}
-    >
-      <div className={styles.addIcon}></div>
-      <div className={styles.btnTxt}>New Challenge</div>
+    <button className={styles.button} onClick={handleOpenModal}>
+      <span>
+        {children}
+        <FiPlus className={styles.icon} />
+      </span>
     </button>
   );
+};
+
+AddBtn.propTypes = {
+  children: PropTypes.node,
+  handleOpenModal: PropTypes.func.isRequired,
 };
 
 export default AddBtn;
