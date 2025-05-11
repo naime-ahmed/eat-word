2b3c6ec30fed3669dde63ba48c10a70536c8b96c
naime-ahmed <naime2molla@gmail.com>
2025-05-11 23:17:48 +0600

    feat: implement a all purpose rate limiter config

--- back-end/utils/rateLimitersConfig.js
+++ back-end/utils/rateLimitersConfig.js
@@ -24,18 +24,39 @@ export const wordLimiterTire = {
 };
 
 // word info gen limiter config
-const createWordInfoGenLimiter = (points, memoryBlock) => new RateLimiterMongo({
-  storeClient: clientPromise,
-  dbName: 'eatWord',
-  tableName: 'wordInfoGenRateLimits',
-  keyPrefix: 'wordInfoGen',
-  points,
-  duration: 24 * 60 * 60, // 24h in seconds
-  inmemoryBlockOnConsumed: memoryBlock,
-  blockDuration: 0,
-});
+const createWordInfoGenLimiter = (points, memoryBlock) =>
+  new RateLimiterMongo({
+    storeClient: clientPromise,
+    dbName: "eatWord",
+    tableName: "wordInfoGenRateLimits",
+    keyPrefix: "wordInfoGen",
+    points,
+    duration: 24 * 60 * 60, // 24h in seconds
+    inmemoryBlockOnConsumed: memoryBlock,
+    blockDuration: 0,
+  });
 
 export const wordInfoGenLimiterTire = {
   regular: createWordInfoGenLimiter(30, 31),
-  pro: createWordInfoGenLimiter(3000, 3001)
+  pro: createWordInfoGenLimiter(3000, 3001),
 };
+
+export function createRateLimiter(key, coll, points = 5, duration = 60 * 60, blockDuration = 60 * 60) {
+  return new RateLimiterMongo({
+    storeClient: clientPromise,
+    dbName: "eatWord",
+    tableName: coll,
+    keyPrefix: `rl:${key}:`,
+    points,
+    duration,
+    blockDuration,
+    inmemoryBlockOnConsumed: false,
+  });
+}
+
+// instantiate specific limiters
+export const loginFailureLimiter = createRateLimiter("login", "loginRateLimits");
+export const forgotPasswordLimiter = createRateLimiter("forgotPass", "forgotPwdRateLimits");
+export const resetPasswordLimiter = createRateLimiter("resetPass", "resetPwdRateLimits" );
+export const updateUserInfoLimiter = createRateLimiter("updateUserInfo", "updateUserInfoLimits", 10);
+
