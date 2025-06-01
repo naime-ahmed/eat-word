8f062d3c5363b5320e4f30a29c90c7e27951cf16
naime-ahmed <naime2molla@gmail.com>
2025-06-02 01:55:08 +0600

    refactor: add props type and add disabled props to primary button

--- front-end/src/components/ui/button/PrimaryBtn/PrimaryBtn.jsx
+++ front-end/src/components/ui/button/PrimaryBtn/PrimaryBtn.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import styles from "./PrimaryBtn.module.css";
 
 const PrimaryBtn = ({
@@ -8,6 +9,7 @@ const PrimaryBtn = ({
   colorTwo = "#0c80c9",
   children,
   btnType = "",
+  disabled = false,
 }) => {
   return (
     <button
@@ -17,11 +19,24 @@ const PrimaryBtn = ({
         backgroundImage: `linear-gradient(to left, ${colorOne}, ${colorTwo})`,
       }}
       className={styles.btn}
-      disabled={isLoading}
+      disabled={disabled}
+      aria-disabled={isLoading}
+      aria-busy={isLoading}
     >
       {isLoading ? loadingText : children}
     </button>
   );
+};
+
+PrimaryBtn.propTypes = {
+  handleClick: PropTypes.func,
+  isLoading: PropTypes.bool,
+  loadingText: PropTypes.string,
+  colorOne: PropTypes.string,
+  colorTwo: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  btnType: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 export default PrimaryBtn;
