f9234079d94667a59e7c535a2bc0213f5e3e2fb3
naime-ahmed <naime2molla@gmail.com>
2025-06-16 23:22:36 +0600

    feat: develope useFetch that send message to chrome with API endpoint

---
+++ extension/chrome/src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await chrome.runtime.sendMessage({
+          type: 'api-request',
+          url
+        });
+        console.log("res: ",response);
+
+        if (response && response.error) {
+            throw new Error(response.error);
+        }
+        
+        setData(response);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
