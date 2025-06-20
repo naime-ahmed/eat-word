c41a0df3d33614773a883a3960717e314da5528e
naime-ahmed <naime2molla@gmail.com>
2025-06-20 23:23:56 +0600

    feat: add build configurationg

--- extension/chrome/vite.config.js
+++ extension/chrome/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        background: 'public/background.js',
+        content: 'public/content.js',
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+});
