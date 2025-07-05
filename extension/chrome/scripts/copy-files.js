c30c4ae84964c9db087d3c7e35283063ded592d0
naime-ahmed <naime2molla@gmail.com>
2025-07-05 22:47:30 +0600

    include defaultpopup files

--- extension/chrome/scripts/copy-files.js
+++ extension/chrome/scripts/copy-files.js
@@ -4,7 +4,9 @@ import path from "path";
 // Files to copy from /public to /dist
 const filesToCopy = [
   { from: "public/manifest.json", to: "dist" },
-  { from: "popup.html", to: "dist" },
+  { from: "defaultPopup.html", to: "dist" },
+  { from: "defaultPopup.css", to: "dist" },
+  { from: "defaultPopup.js", to: "dist" },
   { from: "public/icon-16.png", to: "dist" },
   { from: "public/icon-48.png", to: "dist" },
   { from: "public/icon-128.png", to: "dist" },
