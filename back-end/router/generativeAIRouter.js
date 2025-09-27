27a4e95647a24eba4a8f9203902e710852980b12
naime-ahmed <naime2molla@gmail.com>
2025-09-27 23:31:43 +0600

    add post route for milestone quizzes

--- back-end/router/generativeAIRouter.js
+++ back-end/router/generativeAIRouter.js
@@ -2,6 +2,7 @@
 import express from "express";
 
 // internal imports
+import { generateMilestoneQuiz } from "../controller/generativeAIController/GenQuizController.js";
 import { generateMilestoneStory } from "../controller/generativeAIController/storyWithWordsController.js";
 import { generateWordInfo } from "../controller/generativeAIController/wordInfoController.js";
 import { wordInfoGenLimit } from "../middlewares/rateLimiter/wordInfoGenLimit.js";
@@ -12,5 +13,7 @@ const router = express.Router();
 router.patch("/word-info/:wordId", verifyAccessToken, wordInfoGenLimit,  generateWordInfo);
 
 router.patch("/story/:milestoneId", verifyAccessToken, generateMilestoneStory);
+
+router.post("/quizzes/:milestoneId", verifyAccessToken,  generateMilestoneQuiz);
 
 export default router;
